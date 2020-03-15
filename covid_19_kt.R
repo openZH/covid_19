642d061d48fecd1166933a59f52079499bce785b
tlorusso <th.lorusso@gmail.com>
2020-03-15 12:40:03 +0100

    scraping-script modified

--- covid_19_kt.R
+++ covid_19_kt.R
@@ -3,25 +3,47 @@
 require(dplyr)
 require(rvest)
 
-options(stringsAsFactors = FALSE)
-
 #URL of the BAG page containing the case numbers
-pg <- read_html('https://www.bag.admin.ch/bag/de/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/situation-schweiz-und-international.html')
+pg <- read_html('https://www.bag.admin.ch/bag/en/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/situation-schweiz-und-international.html')
 
 #Date
 datum <- pg %>%
-  html_node(xpath='/html/body/div[2]/div[3]/div/div[2]/div/div[1]/div[5]/article/p[2]/b') %>% 
-  html_text() %>% 
-  
+  html_node(xpath='/html/body/div[2]/div[3]/div/div[2]/div/div[1]/div[5]/article/p[1]/b') %>% 
+  html_text() 
   
 #Table containing the case numbers
 corona_kt <- pg %>% 
     html_node(xpath='/html/body/div[2]/div[3]/div/div[2]/div/div[1]/div[7]/div/div/div/table') %>% 
     html_table()
+
+colnames(corona_kt) <- c("canton","tested_pos","confirmed")
   
 # Add date to table 
-data <- corona_kt %>% mutate(datum=datum)
+data <- corona_kt %>% 
+        mutate(date=datum) %>% 
+        tidyr::separate(date,c("date","time"),sep=",") %>% 
+        mutate(date=as.Date(date, format="%d.%m.%Y"),time=gsub(":","",time))
+
+#load timeseries
+timeseries <- read.csv("COVID19_Cases_Cantons_CH_total.csv", sep=",") %>%
+              mutate(date=as.Date(date)) %>% 
+              filter(date!="2020-03-14")
+
+#if date not already available add data
+if(!unique(data$date) %in% unique(timeseries$date)){
+
+all2 <- timeseries %>% 
+        bind_rows(data) %>% 
+        arrange(canton)
 
 # store as csv
-write.csv(data, "COVID19_Cases_Cantons_CH_total.csv")
+write.csv(all, "COVID19_Cases_Cantons_CH_total.csv",row.names = F)
+
+}
+
+
+
+
+
+
 
