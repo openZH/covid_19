38436990b5b90f283374c2992fee0ec35317dfad
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2020-03-22 23:29:25 +0100

    Fix scraper for BL
    There is some weird character in todays message, which doesn't look to be
    correct UTF-8 codepoint, and it confuses grep and sed. Hack it to remove it,
    and force grep to continue anyway.

--- scrapers/scrape_bl.sh
+++ scrapers/scrape_bl.sh
@@ -6,15 +6,18 @@ URL=$(curl --silent https://www.baselland.ch/politik-und-behorden/direktionen/vo
 d=$(curl --silent "${URL}")
 
 #  <p><span>Der Kantonale Krisenstab gibt Stand Dienstag, 17. März 2020, 14.00 Uhr, 13 neue positive Fälle von Personen mit Wohnsitz im Kanton Basel-Landschaft bekannt. Es sind jetzt insgesamt 89 bestätigte Fälle im Kanton Basel-Landschaft zu verzeichnen. </span></p>
+#  <p>Der Kantonale Krisenstab gibt Stand Sonntag, 22. März 2020, 14.00 Uhr,� sieben neue positive Fälle von Personen mit Wohnsitz im Kanton Basel-Landschaft bekannt. Es sind jetzt insgesamt 289 bestätigte Fälle im Kanton Basel-Landschaft zu verzeichnen. Die Anzahl im Kanton Basel-Landschaft am Coronavirus verstorbener Personen beläuft sich auf insgesamt drei.</p>
+
+# Weird character after Uhr,: chr(0xc2), 'Â'  (302 octal)
 
 # Es sind jetzt insgesamt 89 bestätigte Fälle
 # Es sind jetzt insgesamt 282 bestätigte Fälle
-# Note: There are some non-breaking spaces (ASCII 160) in between some words / numbers.
+# Note: There are some non-breaking spaces (ASCII 160 decimal, 240 octal) in between some words / numbers.
 
 echo "Scraped at: $(date --iso-8601=seconds)"
 
 echo -n "Date and time: "
-echo "$d" | egrep "Stand" | tr '\240' ' ' | sed -E -e 's/^.*Stand [[:alpha:]]+, (.+ Uhr), .*$/\1/'
+echo "$d" | tr '\302' ' ' | egrep --text "Stand" | tr '\240' ' ' | sed -E -e 's/^.*Stand [[:alpha:]]+, (.+ Uhr),.*$/\1/'
 
 echo -n "Confirmed cases: "
-echo "$d" | egrep "Es sind jetzt insgesamt" | tr '\240' ' ' | sed -E -e 's/^.*Es sind jetzt insgesamt ([0-9]+) bestätigte Fälle.*$/\1/'
+echo "$d" | tr '\302' ' ' | egrep --text "Es sind jetzt insgesamt" | tr '\240' ' ' | sed -E -e 's/^.*Es sind jetzt insgesamt ([0-9]+) bestätigte Fälle.*$/\1/'
