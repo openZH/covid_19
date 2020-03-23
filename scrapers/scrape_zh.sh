62dcfbfbe4241dc47141191c98ca60d3ca53b618
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2020-03-24 00:07:47 +0100

    Re-add accidently removed 'Scraped at:' from ZH scraper

--- scrapers/scrape_zh.sh
+++ scrapers/scrape_zh.sh
@@ -3,6 +3,7 @@ set -e
 
 echo ZH
 d=$(./download.sh "https://gd.zh.ch/internet/gesundheitsdirektion/de/themen/coronavirus.html" | egrep "Im Kanton Zürich sind zurzeit|\\(Stand")
+echo "Scraped at: $(date --iso-8601=seconds)"
 
 echo -n "Date and time: "
 echo "$d" | grep "Stand" | sed -E -e 's/.*Stand (.+) Uhr.*/\1/'
