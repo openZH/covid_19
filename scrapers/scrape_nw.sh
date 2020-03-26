1facfb2f5963b459ea2b58bcce935bf3480f0e22
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2020-03-26 09:33:48 +0100

    Update NW scraper for new page format
    New page format.
    
    Add information about deaths.
    
    Closes: https://github.com/openZH/covid_19/issues/201

--- scrapers/scrape_nw.sh
+++ scrapers/scrape_nw.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$(cd "$(dirname "$0")" && pwd)"  # " # To make editor happy
 
 echo NW
-d=$("${DIR}/download.sh" "https://www.nw.ch/gesundheitsamtdienste/6044" | egrep "Stand:|Bisher (ist bei|sind)")  # " # To make my editor happy.
+d=$("${DIR}/download.sh" "https://www.nw.ch/gesundheitsamtdienste/6044" | egrep "Stand:|Bisher (ist bei|sind)|Positiv getestete Personen:|Am Virus verstorbene Personen:")  # " # To make my editor happy.
 echo "Scraped at: $(date --iso-8601=seconds)"
 
 # <p class="icmsPContent icms-wysiwyg-first"><em>Stand: 21.&nbsp;März 2020, 18.15&nbsp; Uhr</em></p>
@@ -14,8 +14,24 @@ echo "Scraped at: $(date --iso-8601=seconds)"
 # <p class="icmsPContent">Bisher sind 42&nbsp;Personen&nbsp;im Kanton Nidwalden positiv auf das Coronavirus getestet worden.&nbsp;<a id="Veranstaltungen" name="Veranstaltungen"></a></p>
 
 
+
+# 2020-03-26
+#            <div class="icms-text-container"><div class="icms-wysiwyg"><h2 class="icmsH2Content"><strong>Aktuelle Situation Kanton Nidwalden</strong></h2>
+#
+#<p class="icmsPContent icms-wysiwyg-first"><em>Stand: 25.&nbsp;März 2020, 15.30 Uhr</em></p>
+
+# <h3 class="icmsH3Content"><br />
+# <strong>Anzahl&nbsp;Erkrankungen/Tote</strong></h3>
+
+# <p class="icmsPContent">Positiv getestete Personen: 44<br />
+# Am Virus verstorbene Personen: 0&nbsp;<a id="Veranstaltungen" name="Veranstaltungen"></a></p>
+
+
 echo -n "Date and time: "
 echo "$d" | grep "Stand:" | sed -E -e 's/^.*em>Stand: *([^<]+)<\/em>.*$/\1/'
 
 echo -n "Confirmed cases: "
-echo "$d" | egrep "Bisher (ist|sind)" | sed -E -e 's/^.*(ist bei|sind) ([0-9]+)(&nbsp;| )Pers.*$/\2/'
+echo "$d" | egrep "Bisher (ist|sind)|Positiv getestete Personen:" | sed -E -e 's/^.*(ist bei|sind) ([0-9]+)(&nbsp;| )Pers.*$/\2/' -e 's/^.*Positiv getestete Personen: ([0-9]+)<.*$/\1/'
+
+echo -n "Deaths: "
+echo "$d" | egrep "Am Virus verstorbene Personen:" | sed -E -e 's/^.*Am Virus verstorbene Personen: ([0-9]+)[^0-9].*$/\1/'
