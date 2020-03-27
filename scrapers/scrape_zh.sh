4bbb6973108fba7fb5c50dd45dd21a3036eb2608
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2020-03-28 00:16:22 +0100

    Scrape more information (hospitalized, ventilated) for ZH
    Closes: https://github.com/openZH/covid_19/issues/283

--- scrapers/scrape_zh.sh
+++ scrapers/scrape_zh.sh
@@ -5,7 +5,7 @@ import scrape_common as sc
 print('ZH')
 d = sc.download("https://gd.zh.ch/internet/gesundheitsdirektion/de/themen/coronavirus.html")
 sc.timestamp()
-d = sc.filter(r"Im Kanton Zürich sind zurzeit|\(Stand|Total ([0-9]+) Todesfälle", d)
+d = sc.filter(r"Im Kanton Zürich sind zurzeit|\(Stand|Total ([0-9]+) Todesfälle|Spitalbehandlung|beatmet", d)
 #                                 <h2>Aktuelle Situation im Kanton Zürich (24.3.2020, 9.30 Uhr)</h2>
 #                         
 #                         
@@ -27,9 +27,29 @@ d = sc.filter(r"Im Kanton Zürich sind zurzeit|\(Stand|Total ([0-9]+) Todesfäll
 <p>(Stand 26.3.2020, 9.30 Uhr)</p>
 """
 
+# 2020-03-27
+"""
+                                <h2>Aktuelle Situation im Kanton Zürich (27.3.2020, 9.30 Uhr)</h2>
+...
+                        <p>Im Kanton Zürich sind zurzeit 1578 Personen positiv auf das Coronavirus getestet worden.</p>
+<p>156 positiv Getestete befinden sich in Spitalbehandlung, davon werden 37 künstlich beatmet.</p>
+<p>Total 11 Todesfälle (75-jährig, 78, 78, 78, 80, 80, 85, 88, 90, 96, 97).</p>
+<p>Um für den erwarteten Ansturm auf die Spitäler auch personell gerüstet zu sein, hat die Gesundheitsdirektion einen Pool für Gesundheitsfachpersonal eingerichtet. Spitäler können darüber rasch und unkompliziert zusätzliches Personal anfordern und die medizinische Versorgung so auch bei einem rasanten Anstieg an hospitalisierten Corona-Fällen sicherstellen. Gesundheitsfachpersonal kann sich ab sofort <a href="https://www.careanesth.com/covid-19/pool" target="_blank" rel="noopener noreferrer">hier bewerben.</a></p>
+<p>Die Gesundheitsdirektion beschafft sich eine Maschine, die täglich automatisch bis zu 32'000 FFP2-Schutzmasken herstellen kann. In der zweiten Hälfte des Monats April ist die Maschine betriebsbereit.&nbsp;</p>
+<p>(Stand 27.3.2020, 9.30 Uhr)</p>
+"""
+
 print("Date and time:", sc.find('Stand (.+) Uhr', d))
 print("Confirmed cases:", sc.find('Im .* Zürich .* ([0-9]+) Person(en)? posit', d))
 deaths = sc.find('Im .* Zürich .* Total ([0-9]+) Todesfälle', d)
 if not deaths:
   deaths = sc.find('Total ([0-9]+) Todesfälle', d)
 print("Deaths:", deaths)
+
+hospitalized = sc.find('>([0-9]+) positiv Getestete befinden sich in Spitalbehandlung', d)
+if hospitalized:
+  print('Hospitalized:', hospitalized)
+
+ventilated = sc.find('davon werden ([0-9]+) künstlich beatmet', d)
+if ventilated:
+  print('Vent:', ventilated)
