4a8786edcd15f067f4fe5fcb9b67faffa60e61ee
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2020-03-31 20:51:41 +0200

    Fix NW scraper
    Minor format changes.
    
    Relax regexps to match more character here and there.
    
    Closes: https://github.com/openZH/covid_19/issues/346

--- scrapers/scrape_nw.sh
+++ scrapers/scrape_nw.sh
@@ -5,7 +5,9 @@ import scrape_common as sc
 print('NW')
 d = sc.download('https://www.nw.ch/gesundheitsamtdienste/6044')
 sc.timestamp()
-d = sc.filter(r'Stand:|Bisher (ist bei|sind)|Positiv getestete Personen:|Am Virus verstorbene Personen:', d)
+d = d.replace('&nbsp;', ' ')
+d = d.replace('<strong>', ' ').replace('</strong>', ' ')
+# d = sc.filter(r'Stand:|Bisher (ist bei|sind)|Positiv getestete Personen:|Am Virus verstorbene Personen:', d)
 
 # <p class="icmsPContent icms-wysiwyg-first"><em>Stand: 21.&nbsp;März 2020, 18.15&nbsp; Uhr</em></p>
 # <p class="icmsPContent">Bisher ist bei 33&nbsp;Personen&nbsp;im Kanton Nidwalden das Coronavirus nachgewiesen worden.&nbsp;<a id="Veranstaltungen" name="Veranstaltungen"></a></p>
@@ -33,10 +35,18 @@ d = sc.filter(r'Stand:|Bisher (ist bei|sind)|Positiv getestete Personen:|Am Viru
  Am Virus verstorbene Personen: 0&nbsp;<a id="Veranstaltungen" name="Veranstaltungen"></a></p>
 """
 
+# 2020-03-31
+"""
+<strong>Anzahl Fälle</strong></h3>
+
+<p class="icmsPContent">Positiv getestete Personen: <strong>70&nbsp;</strong>(+7)<br />
+Verstorbene Personen:&nbsp;<strong>0</strong></p>
+"""
+
 print('Date and time:', sc.find(r'em>Stand: *([^<]+)<\/em>', d))
 
 a = sc.find(r'Bisher (ist bei|sind) ([0-9]+)(&nbsp;| )Pers', d, group=2)
-b = sc.find(r'Positiv getestete Personen: ([0-9]+)<', d)
+b = sc.find(r'Positiv\s*getestete\s*Personen:\s*([0-9]+)\b', d)
 print('Confirmed cases:', a or b)
 
-print('Deaths:', sc.find(r'Am Virus verstorbene Personen: ([0-9]+)[^0-9]', d))
+print('Deaths:', sc.find(r'(?:Am\s*Virus\s*)?verstorbene\s*Persone?n?:\s*([0-9]+)\b', d))
