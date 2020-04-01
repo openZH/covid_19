9cd3ff5aa33c459589b801f4278513cb04659626
piotrpiotrowski <piotrpiotrowski87@gmail.com>
2020-04-01 19:15:36 +0200

    Reimplement the LU scraper
    HTML describing statistics format was changed so the previous implementation could not find and parse them

--- scrapers/scrape_lu.sh
+++ scrapers/scrape_lu.sh
@@ -1,23 +1,65 @@
 #!/usr/bin/env python3
 
 import scrape_common as sc
+import datetime
+
+def extractStatisticByName(statistics, label):
+    index = [statistics.index(line) + 3 for line in statistics if label in line][0]
+    return statistics[index].replace("""<p style="text-align: right;">""", "").replace("</p>", "")
 
 print('LU')
 d = sc.download('https://gesundheit.lu.ch/themen/Humanmedizin/Infektionskrankheiten/Coronavirus')
 sc.timestamp()
 
-# 2020-03-25
 """
-        <p class="teaser__text richtext"><p>Im Kanton Luzern gibt es 228 best&auml;tigte F&auml;lle (Stand: 25. M&auml;rz 2020, 11:00 Uhr).&nbsp; Es gibt zwei Todesf&auml;lle im Zusammenhang dem Coronavirus zu beklagen. </p>
+<p><strong>Aktuelle Fallzahlen im Kanton Luzern&nbsp;</strong>(Stand: 1. April 2020, 11:00 Uhr)</p>
+<table border="0" cellspacing="0" cellpadding="0">
+    <tbody>
+        <tr>
+            <td valign="top" style="width: 151px;">
+            <p><strong></strong>Bestätigte Fälle: </p>
+            </td>
+            <td valign="top" style="width: 47px;">
+            <p style="text-align: right;">401</p>
+            </td>
+        </tr>
+        <tr>
+            <td valign="top" style="width: 151px;">
+            <p>Hospitalisiert:</p>
+            </td>
+            <td valign="top" style="width: 47px;">
+            <p style="text-align: right;">57</p>
+            </td>
+        </tr>
+        <tr>
+            <td valign="top" style="width: 151px;">
+            <p>Intensivpflege:</p>
+            </td>
+            <td valign="top" style="width: 47px;">
+            <p style="text-align: right;">12</p>
+            </td>
+        </tr>
+        <tr>
+            <td valign="top" style="width: 151px;">
+            <p>Todesfälle: </p>
+            </td>
+            <td valign="top" style="width: 47px;">
+            <p style="text-align: right;">7</p>
+            </td>
+        </tr>
+    </tbody>
+</table>
 """
 
-d = sc.filter(r'Im Kanton Luzern gibt es', d)
+lines = d.split('\n')
+statisticsHeaderLine = [x for x in lines if "Aktuelle Fallzahlen im Kanton Luzern" in x][0]
+beginingOfStatisticsBlock = lines.index(statisticsHeaderLine)
+statisticsLines = lines[beginingOfStatisticsBlock:beginingOfStatisticsBlock + 36]
+dateAsString = statisticsHeaderLine.replace("""<p><strong>Aktuelle Fallzahlen im Kanton Luzern&nbsp;</strong>(Stand: """, "").replace(" Uhr)</p>", "")
+date = datetime.datetime.strptime(dateAsString, '%d. %B %Y, %H:%M')
 
-print('Date and time:', sc.find(r'Stand: (.+)(Uhr)?\)', d))
-print('Confirmed cases:', sc.find('gibt es ([0-9]+) best(&auml;|ä)tigte F(&auml;|ä)lle', d))
-
-deathsString = sc.find('Es gibt (.*) Todesf(&auml;|ä)lle', d)
-
-deaths = sc.int_or_word(deathsString)
-if not deaths is None:
-  print('Deaths:', deaths)
+print('Date and time:', date.strftime("%Y-%m-%d %H:%M"))
+print('Confirmed cases:', extractStatisticByName(statisticsLines, "Bestätigte Fälle:"))
+print('Deaths:', extractStatisticByName(statisticsLines, "Todesfälle:"))
+print('Hospitalized:', extractStatisticByName(statisticsLines, "Hospitalisiert:"))
+print('ICU:', extractStatisticByName(statisticsLines, "Intensivpflege:"))
