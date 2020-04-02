462347e9bcb49710ec912b2532e61892956c025a
dr3tt <24375656+dr3tt@users.noreply.github.com>
2020-04-02 10:39:25 +0200

    Update SO scraper (deaths counter)

--- scrapers/scrape_so.sh
+++ scrapers/scrape_so.sh
@@ -11,7 +11,22 @@ d = sc.filter("Situation Kanton Solothurn.*Stand|Anzahl positiv getesteter Erkra
 """
  <p class="bodytext"><strong>Situation Kanton Solothurn (Stand 23.03.2020, 12:00)</strong></p><ul><li>Anzahl positiv getesteter Erkrankungsfälle: 95 Personen</li> 	<li>Verstorben:<strong> </strong>1 Person</li></ul><p class="bodytext"> </p></div></div>
 """
+# 2020-04-02
+"""
+ <p class="bodytext"><strong>Situation Kanton Solothurn (Stand 02.04.2020, 0:00 Uhr)</strong></p><ul><li>Anzahl positiv getesteter Erkrankungsfälle: 227 (+11 im Vergleich zum Vortag)</li> 	<li>Verstorbene Personen:<strong> </strong>3 (keine Veränderung im Vergleich zum Vortag)</li></ul><p class="bodytext"> </p></div></div>
+"""
 
 print("Date and time:", sc.find(r'\(Stand ([^\)]+)\)<', d))
 print("Confirmed cases:", sc.find(r'Anzahl positiv getesteter Erkrankungsfälle: ([0-9]+) ', d))
-print("Deaths:", sc.find('Verstorben:(<strong> <\/strong>)?([0-9]+) ', d, group=2))
+
+deaths_strings = [
+    'Verstorben:(<strong> <\/strong>)?([0-9]+) ',
+    'Verstorbene Personen:(<strong> <\/strong>)?([0-9]+) ',
+]
+deaths = None
+for deaths_string in deaths_strings:
+    deaths = sc.find(deaths_string, d, group=2)
+    if deaths is not None:
+        break
+
+print("Deaths:", deaths)
