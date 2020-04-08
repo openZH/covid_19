c4a2c596512a914c1f32c941d7e8a6b7dd363909
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-04-08 17:53:34 +0200

    Update merge script to use new files

--- scripts/merge_canton_csvs.rb
+++ scripts/merge_canton_csvs.rb
@@ -3,7 +3,7 @@
 require 'csv'
 
 # get files
-files = Dir["fallzahlen_kanton_total_csv/*.csv"]
+files = Dir["fallzahlen_kanton_total_csv_v2/*.csv"]
 
 # output array
 rows = []
@@ -18,7 +18,7 @@ files.each do |fn|
     if row[1] =~ /(\d{1,2}):(\d{1,2})/
       row[1] = sprintf "%02d:%02d", $1.to_i, $2.to_i
     end
-    rows << row[0..10]
+    rows << row[0..11]
   end
 end
 
