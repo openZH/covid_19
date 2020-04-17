4ab8e9fb8d5bf40f148be2c3d3fdf2970d2f413e
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2020-04-17 14:14:55 +0200

    Adjust xlsx parsing for GL. Extract time of day. Shift columns.
    New column in GL spreadsheet with time of a day.

--- scrapers/scrape_gl.py
+++ scrapers/scrape_gl.py
@@ -19,16 +19,21 @@ sheet = xls.sheet_by_index(0)
 last_row = sheet.nrows - 1
 
 date_value = sheet.cell_value(last_row, 0)
+time_value = sheet.cell_value(last_row, 1)
+import xlrd
 current_date = sc.xldate_as_datetime(sheet, date_value)
-print('Date and time:', current_date.date().isoformat())
+if time_value:
+  print('Date and time:', current_date.date().isoformat(), xlrd.xldate.xldate_as_datetime(time_value, xls.datemode).time().isoformat())
+else:
+  print('Date and time:', current_date.date().isoformat())
 
-cases = int(sheet.cell_value(last_row, 1)) 
+cases = int(sheet.cell_value(last_row, 2))
 print('Confirmed cases:', cases)
 
-hosp = int(sheet.cell_value(last_row, 2))
+hosp = int(sheet.cell_value(last_row, 3))
 if hosp:
     print('Hospitalized:', hosp)
 
-deaths = int(sheet.cell_value(last_row, 3))
+deaths = int(sheet.cell_value(last_row, 4))
 if deaths:
     print('Deaths:', deaths)
