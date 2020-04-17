5058da6324d0ed78d33c06e1dd490d6b076775b5
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2020-04-17 12:07:15 +0200

    Support absolute URL for CSV for ZG scraper. Parse CSV first fully.
    This reworks parsing of the CSV, and only outputs the data fully if all
    data is present for the latest day, otherwise fallback fully to main
    website.
    
    Also, support an absolute URLs.

--- scrapers/scrape_zg.py
+++ scrapers/scrape_zg.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import re
+from bs4 import BeautifulSoup
+import collections
 import csv
 from io import StringIO
-from bs4 import BeautifulSoup
+import re
 import scrape_common as sc
 
 print('ZG')
@@ -12,22 +13,24 @@ sc.timestamp()
 soup = BeautifulSoup(d, 'html.parser')
 d = sc.filter(r'Infizierte Personen|Genesene Personen|Verstorbene Personen|Stand:', d)
 date_time_string = sc.find(r'Stand:? ([^<]+ Uhr)<', d)
-print('Date and time:', date_time_string)
 last_update = None
 matches = re.search(r'(\d+)\.(\d+)\.(\d+)', date_time_string)
-if matches is not None:
+if matches:
     last_update = f"{int(matches[1]):02d}.{int(matches[2]):02d}.{matches[3]}"
 
 detailed_stats = soup.find("a", text=re.compile("Detaillierte Statistik"))
 
-cases = None
-deaths = None
-recovered = None
+found_last_day = False
+
 if detailed_stats and last_update is not None:
     d_detailed_stats = sc.download(detailed_stats['href'])
     csv_path = sc.find(r'csv_path:"([^"]+)"', d_detailed_stats)
-    csv_url = f"https://www.zg.ch/{csv_path}"
+    if csv_path.startswith('/'):
+        csv_url = f"https://www.zg.ch/{csv_path}"
+    else:
+      csv_url = csv_path
     d_csv = sc.download(csv_url)
+    sc.timestamp()
     """
 "Typ","Datum","Anzahl","Meta","Type","Content"
 "Fallzahl","01.03.2020","0",NA,NA,NA
@@ -52,21 +55,26 @@ NA,NA,NA,"1","description","Aufgrund der kleinen Fallzahlen im Kanton Zug könne
 NA,NA,NA,"1","source","Kanton Zug, Amt für Gesundheit"
 """
     reader = csv.DictReader(StringIO(d_csv), delimiter=',')
+    data = collections.defaultdict(dict)
     for row in reader:
-        if row['Datum'] == last_update:
-            if row['Typ'] == 'Fallzahl':
-                cases = int(row['Anzahl'])
-                print('Confirmed cases:', cases)
-            elif row['Typ'] == 'Hospitalisierte':
-                print('Hospitalized:', int(row['Anzahl']))
-            elif row['Typ'] == 'Hospitalisierte in Intensivpflege':
-                print('ICU:', int(row['Anzahl']))
-            elif row['Typ'] == 'Genesene':
-                recovered = int(row['Anzahl'])
-                print('Recovered:', recovered)
-            elif row['Typ'] == 'Todesfälle':
-                deaths = int(row['Anzahl'])
-                print('Deaths:', deaths)
+        if row['Typ'] == 'NA' or row['Datum'] == 'NA':
+            continue
+        data[row['Datum']][row['Typ']] = row['Anzahl']
+    days = list(data.keys())
+    last_day = data[days[-1]]
+    if (last_day == last_update and
+        'Fallzahl' in last_day and
+        'Hospitalisierte' in last_day and
+        'Hospitalisierte in Intensivpflege' in last_day and
+        'Genesene' in last_day and
+        'Todesfälle' in last_day):
+        found_last_day = True
+        print('Date and time', days[-1])
+        print('Confirmed cases:', last_day['Fallzahl'])
+        print('Hospitalized:', last_day['Hospitalisierte'])
+        print('ICU:', last_day['Hospitalisierte in Intensivpflege'])
+        print('Recovered:', last_day['Genesene'])
+        print('Deaths:', last_day['Todesfälle'])
 
 # 2020-03-23
 """
@@ -75,9 +83,8 @@ NA,NA,NA,"1","source","Kanton Zug, Amt für Gesundheit"
 <p>Verstorbene Personen: 0</p>
 <p>Stand: 23.3.2020, 8.00 Uhr</p>
 """
-if cases is None:
+if not found_last_day:
+    print('Date and time:', date_time_string)
     print('Confirmed cases:', sc.find(r'Infizierte Personen:? ([0-9]+)<', d))
-if deaths is None:
     print('Deaths:', sc.find(r'Verstorbene Personen:? ([0-9]+)<', d))
-if recovered is None:
     print('Recovered:', sc.find(r'Genesene Personen:? ([0-9]+)<', d))
