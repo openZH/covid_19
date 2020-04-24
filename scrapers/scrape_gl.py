eea70c4a2d1760844600036120914341c46ff31d
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-04-25 01:03:39 +0200

    Adapt GL scraper for time series

--- scrapers/scrape_gl.py
+++ scrapers/scrape_gl.py
@@ -5,21 +5,28 @@ import re
 from bs4 import BeautifulSoup
 import scrape_common as sc
 
-print('GL')
-d = sc.download('https://www.gl.ch/verwaltung/finanzen-und-gesundheit/gesundheit/coronavirus.html/4817')
-sc.timestamp()
+d = sc.download('https://www.gl.ch/verwaltung/finanzen-und-gesundheit/gesundheit/coronavirus.html/4817', silent=True)
 
 
 soup = BeautifulSoup(d, 'html.parser')
 box = soup.find('div', class_="box--error")
 xls_url = box.find('a', string=re.compile(r'.*Dokument.*')).get('href')
-xls = sc.xlsdownload(xls_url)
-sc.timestamp()
 
+xls = sc.xlsdownload(xls_url, silent=True)
 rows = sc.parse_xls(xls)
-if rows:
-    last_row = rows[-1]
-    print('Date and time:', last_row['Datum'].date().isoformat(), last_row['Zeit'].time().isoformat())
-    print('Confirmed cases:', last_row['Bestätigte Fälle (kumuliert)'])
-    print('Hospitalized:', last_row['Personen in Spitalpflege'])
-    print('Deaths:', last_row['Todesfälle (kumuliert)'])
+for i, row in enumerate(rows):
+    print('GL')
+    sc.timestamp()
+    print('Downloading:', xls_url)
+    if row['Zeit']:
+        print('Date and time:', row['Datum'].date().isoformat(), row['Zeit'].time().isoformat())
+    else:
+        print('Date and time:', row['Datum'].date().isoformat())
+    print('Confirmed cases:', row['Bestätigte Fälle (kumuliert)'])
+    print('Hospitalized:', row['Personen in Spitalpflege'])
+    print('Deaths:', row['Todesfälle (kumuliert)'])
+    # do not print record delimiter for last record
+    # this is an indicator for the next script to check
+    # for expected values.
+    if len(rows) - 1 > i:
+        print('-' * 10)
