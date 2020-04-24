02f31e3a9918c1cab21be650b673004fd82d0690
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-04-25 00:47:13 +0200

    Rewrite NW scraper for time series

--- scrapers/scrape_nw.py
+++ scrapers/scrape_nw.py
@@ -3,15 +3,20 @@
 
 import scrape_common as sc
 
-print('NW')
-xls = sc.xlsdownload('http://www.nw.ch/coronastatistik')
-sc.timestamp()
-
+xls_url = 'http://www.nw.ch/coronastatistik'
+xls = sc.xlsdownload(xls_url, silent=True)
 rows = sc.parse_xls(xls, header_row=3)
-if rows:
-    last_row = rows[-1]
-    print('Date and time:', last_row['A'].date().isoformat())
-    print('Confirmed cases:', last_row['Positiv getestete Personen (kumuliert)'])
-    print('Hospitalized:', last_row['Aktuell hospitalisierte Personen'])
-    print('ICU:', last_row['Davon auf der Intensivstation'])
-    print('Deaths:', last_row['Personen verstorben'])
+for i, row in enumerate(rows):
+    print('NW')
+    sc.timestamp()
+    print('Downloading:', xls_url)
+    print('Date and time:', row['A'].date().isoformat())
+    print('Confirmed cases:', row['Positiv getestete Personen (kumuliert)'])
+    print('Hospitalized:', row['Aktuell hospitalisierte Personen'])
+    print('ICU:', row['Davon auf der Intensivstation'])
+    print('Deaths:', row['Personen verstorben'])
+    # do not print record delimiter for last record
+    # this is an indicator for the next script to check
+    # for expected values.
+    if len(rows) - 1 > i:
+        print('-' * 10)
