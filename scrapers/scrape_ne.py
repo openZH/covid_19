943c1103f4e265cc3f6b6ee356107cef2333fb10
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-04-25 01:39:40 +0200

    Adapt NE scraper for time series

--- scrapers/scrape_ne.py
+++ scrapers/scrape_ne.py
@@ -3,17 +3,22 @@
 
 import scrape_common as sc
 
-print('NE')
-
 xls_url = 'https://www.ne.ch/autorites/DFS/SCSP/medecin-cantonal/maladies-vaccinations/Documents/Covid-19-Statistiques/COVID19_PublicationInternet.xlsx'
-xls = sc.xlsdownload(xls_url)
-sc.timestamp()
+xls = sc.xlsdownload(xls_url, silent=True)
 rows = sc.parse_xls(xls)
-if rows:
-    last_row = rows[-1]
-    print('Date and time:', last_row['A'].date().isoformat())
-    print('Confirmed cases:', last_row['Cumul'])
-    print('Hospitalized:', last_row['Total des cas hospitalisés'])
-    print('ICU:', int(last_row['Soins intensifs (intubés)']) + int(last_row['Soins intensifs (non intubés)']))
-    print('Vent:', last_row['Soins intensifs (intubés)'])
-    print('Deaths:', last_row['Cumul des décès'])
+for i, row in enumerate(rows):
+    print('NE')
+    sc.timestamp()
+    print('Downloading:', xls_url)
+    print('Date and time:', row['A'].date().isoformat())
+    print('Confirmed cases:', row['Cumul'])
+    print('Hospitalized:', row['Total des cas hospitalisés'])
+    if row['Soins intensifs (intubés)'] and row['Soins intensifs (non intubés)']:
+        print('ICU:', int(row['Soins intensifs (intubés)']) + int(row['Soins intensifs (non intubés)']))
+    print('Vent:', row['Soins intensifs (intubés)'])
+    print('Deaths:', row['Cumul des décès'])
+    # do not print record delimiter for last record
+    # this is an indicator for the next script to check
+    # for expected values.
+    if len(rows) - 1 > i:
+        print('-' * 10)
