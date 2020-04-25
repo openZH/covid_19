3b61f1a60d73090c6174395cad29cedcce8d7d04
Dominik Gehl <dominik.gehl@contracted.pmi.com>
2020-04-25 17:09:15 +0200

    extracting case numbers for previous days in AG

--- scrapers/scrape_ag.py
+++ scrapers/scrape_ag.py
@@ -3,7 +3,6 @@
 import scrape_common as sc
 import re
 
-print('AG')
 
 # get latest from list with all bulletins
 d = sc.download('https://www.ag.ch/de/themen_1/coronavirus_2/lagebulletins/lagebulletins_1.jsp')
@@ -11,9 +10,24 @@ d = sc.download('https://www.ag.ch/de/themen_1/coronavirus_2/lagebulletins/lageb
 url = sc.find(r'<a [^>]*href="([^"]+\.pdf)">.+Bulletin.+</a>', d)
 
 # download latest PDF
-d = sc.pdfdownload('https://www.ag.ch' + url, raw=True)
+pdf_url = 'https://www.ag.ch' + url
+d = sc.pdfdownload(pdf_url, raw=True)
+# extract case numbers reported for previous days
+data_rows = [row for row in d.split("\n")
+             if re.search(r'^(Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag),\s+\d{2}\.\d{2}\.\d{4}\s+[\'’0-9]+$', row)]
+for row in data_rows[0:-1]:
+    print('AG')
+    sc.timestamp()
+    m = re.search(r'^(.+)\s+([0-9]+)$', re.sub(r'[\'’]', '', row))
+    print('Downloading:', pdf_url)
+    print('Date and time:', m[1])
+    print('Confirmed cases:', m[2])
+    print('-' * 10)
 
+# and now the latest data for the current day
+print('AG')
 sc.timestamp()
+print('Downloading:', pdf_url)
 
 print('Date and time:', sc.find(r'Aarau, (.+? Uhr)', d))
 
