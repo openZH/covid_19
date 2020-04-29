ad65db07ec5bc3f2cd6797916b0b51a4d63b20ea
Dominik Gehl <dominik.gehl@contracted.pmi.com>
2020-04-29 16:00:50 +0200

    updating NW

--- scrapers/scrape_nw.py
+++ scrapers/scrape_nw.py
@@ -7,14 +7,13 @@ xls_url = 'http://www.nw.ch/coronastatistik'
 xls = sc.xlsdownload(xls_url, silent=True)
 rows = sc.parse_xls(xls, header_row=3)
 for i, row in enumerate(rows):
-    print('NW')
-    sc.timestamp()
-    print('Downloading:', xls_url)
-    print('Date and time:', row['A'].date().isoformat())
-    print('Confirmed cases:', row['Positiv getestete Personen (kumuliert)'])
-    print('Hospitalized:', row['Aktuell hospitalisierte Personen'])
-    print('ICU:', row['Davon auf der Intensivstation'])
-    print('Deaths:', row['Personen verstorben'])
+    dd = sc.DayData(canton='NW', url=xls_url)
+    dd.datetime = row['A'].date().isoformat()
+    dd.cases = row['Positiv getestete Personen (kumuliert)']
+    dd.hospitalized = row['Aktuell hospitalisierte Personen']
+    dd.icu = row['Davon auf der Intensivstation']
+    dd.deaths = row['Personen verstorben']
+    print(dd)
     # do not print record delimiter for last record
     # this is an indicator for the next script to check
     # for expected values.
