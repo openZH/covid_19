564fa215190187d5595538473a283071c1b5de57
Dominik Gehl <dominik.gehl@contracted.pmi.com>
2020-04-29 11:13:53 +0200

    reverting matrix change

--- scrapers/scrape_matrix.py
+++ scrapers/scrape_matrix.py
@@ -23,7 +23,7 @@ matrix = {
     'GR': ['Confirmed cases', 'Deaths', 'Hospitalized'],
     'JU': ['Confirmed cases', 'Deaths', 'Hospitalized', 'ICU'],
     'LU': ['Confirmed cases', 'Deaths', 'Hospitalized', 'ICU'],
-    'NE': ['Confirmed cases', 'Deaths', 'Hospitalized', 'ICU', 'Vent'],
+    'NE': ['Hospitalized', 'Vent'],
     'NW': ['Confirmed cases', 'Deaths', 'Hospitalized', 'ICU'],
     'OW': ['Confirmed cases', 'Deaths', 'Hospitalized'],
     'SG': ['Confirmed cases', 'Deaths', 'Released', 'Hospitalized', 'ICU'],
