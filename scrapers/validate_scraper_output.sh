0036138611412f72fa37adf15db5c8cd1a9d5d72
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-05-12 15:00:35 +0200

    Run validation seprate from scraping
    - If the scraper didn't fail, commit the resulkt
    - validate the output and report back failure to slack channel

---
+++ scrapers/validate_scraper_output.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script to run a single scraper
+
+set -e
+set -o pipefail
+
+function cleanup {
+  exit $?
+}
+trap "cleanup" EXIT
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+
+# SCRAPER_KEY must be set
+if [ -z $SCRAPER_KEY ] ; then
+  echo "SCRAPER_KEY env variable must be set"; 
+  exit 1
+fi
+
+# 1. Validate the result
+node $DIR/../scripts/validate-csv.js $DIR/../fallzahlen_kanton_total_csv_v2/COVID19_Fallzahlen_Kanton_${SCRAPER_KEY}_total.csv
+
+# 2. Check for outliers
+python $DIR/../scripts/check_for_outliers.py $DIR/../fallzahlen_kanton_total_csv_v2/COVID19_Fallzahlen_Kanton_${SCRAPER_KEY}_total.csv
