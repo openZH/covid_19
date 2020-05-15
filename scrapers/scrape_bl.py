860756d5437d26a00554da53fef7d82f078ba904
Dominik Gehl <dominik.gehl@contracted.pmi.com>
2020-05-15 12:56:42 +0200

    Adapting BL parser

--- scrapers/scrape_bl.py
+++ scrapers/scrape_bl.py
@@ -90,7 +90,7 @@ Datum, Geheilte kalkuliert, Aktive Fälle, Todesfälle
 
     d = d.replace('\n', ' ')
     # Extract last line. Use non-greedy matching.
-    data = sc.find(r'<pre id="data".*?> ?Datum, Bestätigte Fälle, Geheilte (?:geschätzt|kalkuliert), (?:Verstorbene|Todesfälle)\s*([^<]+)</pre>', d)
+    data = sc.find(r'<pre id="data[^"]*".*?> ?Datum, Bestätigte Fälle, Geheilte (?:geschätzt|kalkuliert), (?:Verstorbene|Todesfälle)\s*([^<]+)</pre>', d)
     if data:
         for row in data.split(" "):
             c = row.split(',')
@@ -101,7 +101,7 @@ Datum, Geheilte kalkuliert, Aktive Fälle, Todesfälle
             rows[row_date]['deaths'] = c[3]
             rows[row_date]['recovered'] = c[2]
     else:
-        data = sc.find(r'<pre id="data".*?> ?Datum, Normale Station, Intensivstation\s*([^<]+)</pre>', d)
+        data = sc.find(r'<pre id="data[^"]*".*?> ?Datum, Normale Station, Intensivstation\s*([^<]+)</pre>', d)
         if data:
             for row in data.split(" "):
                 c = row.split(',')
@@ -113,7 +113,8 @@ Datum, Geheilte kalkuliert, Aktive Fälle, Todesfälle
                         rows[row_date]['hospitalized'] = int(c[1] or 0) + int(c[2] or 0)
                     rows[row_date]['icu'] = c[2]
         else:
-            data = sc.find(r'<pre id="data".*?> ?Datum, Geheilte kalkuliert, Aktive Fälle, Todesfälle\s*([^<]+)</pre>', d)
+            data = sc.find(r'<pre id="data[^"]*".*?> ?Datum, Geheilte kalkuliert, Aktive Fälle, Todesfälle\s*([^<]+)</pre>', d) or \
+                sc.find(r'<pre id="data_1".*?> ?Datum,&quot;Geheilte \(kalkuliert\)&quot;,&quot;Aktive Fälle \(kalkuliert\)&quot;,&quot;Todesfälle&quot;\s*([^<]+)</pre>', d)
             if data:
                 for row in data.split(" "):
                     c = row.split(',')
@@ -125,6 +126,21 @@ Datum, Geheilte kalkuliert, Aktive Fälle, Todesfälle
                             rows[row_date]['cases'] = int(c[1] or 0) + int(c[2] or 0) + int(c[3] or 0)
                         rows[row_date]['recovered'] = c[1]
                         rows[row_date]['deaths'] = c[3]
+            else:
+                data = sc.find(r'<pre id="data_1".*?> ?Datum,&quot;Normale Station&quot;,&quot;Intensivstation \(nicht beatmet\)&quot;,&quot;Intensivstation \(beatmet\)&quot;\s*([^<]+)</pre>', d)
+                if data:
+                    for row in data.split(" "):
+                        c = row.split(',')
+                        if len(c) == 4:
+                            row_date = c[0].replace('-', '.')
+                            if row_date not in rows:
+                                rows[row_date] = {'date': row_date}
+                            if c[1] or c[2] or c[3]:
+                                rows[row_date]['hospitalized'] = int(float(c[1] or 0) + float(c[2] or 0) + float(c[3] or 0))
+                            rows[row_date]['icu'] = c[2]
+                            rows[row_date]['vent'] = c[3]
+                else:
+                    print("bug in regexp")
 
 is_first = True
 for row_date, row in rows.items():
