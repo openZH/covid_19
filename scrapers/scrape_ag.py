52ce4c9610c7c092e4c13ebac883c29ba2a515e5
Fabian Vogler <fabian@equivalence.ch>
2020-05-28 15:13:10 +0200

    Add isolated and quarantined to parser for AG
    Closes #772

--- scrapers/scrape_ag.py
+++ scrapers/scrape_ag.py
@@ -41,4 +41,6 @@ if dd.vent == 'alle':
     dd.vent = dd.icu
 dd.deaths = sc.find(
     r'([0-9]+)\s+Person(en)?\s+an\s+den\s+Folgen\s+des\s+Coronavirus\s+verstorben', d)
+dd.isolated = sc.find(r'betreut\s+sind\s+([0-9]+)\s+infizierte\s+Personen(en)?', d)
+dd.quarantined = sc.find(r'([0-9]+)\s+enge\s+Kontaktperson(en)?', d)
 print(dd)
