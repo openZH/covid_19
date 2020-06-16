08d6a153aa688a71f21051fc951f9461ddc1d9e6
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-06-16 15:06:44 +0200

    Update source of GR scraper

--- scrapers/scrape_gr.py
+++ scrapers/scrape_gr.py
@@ -1,52 +1,80 @@
 #!/usr/bin/env python3
 
-import json
+import datetime
 import scrape_common as sc
 
-json_url = 'https://www.gr.ch/DE/institutionen/verwaltung/djsg/ga/coronavirus/_layouts/15/GenericDataFeed/feed.aspx?PageID=26&ID=g_1175d522_e609_4287_93af_d14c9efd5218&FORMAT=JSONRAW'
-d = sc.download(json_url, silent=True)
+json_url = 'https://services1.arcgis.com/YAuo6vcW85VPu7OE/arcgis/rest/services/Fallzahlen_Total_Kanton/FeatureServer/0/query?where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnHiddenFields=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Eingangs_Datum&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=standard&f=pjson'
+data = sc.jsondownload(json_url, silent=True)
 
 # 2020-04-02
 """
-[
-  {"date":"2020-03-18","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"116","ncumul_hosp":"13","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"1","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-19","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"145","ncumul_hosp":"18","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"1","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-20","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"213","ncumul_hosp":"24","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"3","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-21","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"239","ncumul_hosp":"24","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"3","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-22","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"266","ncumul_hosp":"27","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"6","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-23","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"281","ncumul_hosp":"29","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"7","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-24","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"322","ncumul_hosp":"43","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"8","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-25","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"373","ncumul_hosp":"45","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"9","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-26","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"373","ncumul_hosp":"45","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"9","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-27","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"409","ncumul_hosp":"52","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"9","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-30","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"497","ncumul_hosp":"63","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"12","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-03-31","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"513","ncumul_hosp":"58","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"19","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-04-01","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"521","ncumul_hosp":"58","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"21","source":"https://www.gr.ch/coronavirus"},
-  {"date":"2020-04-02","time":"","abbreviation_canton_and_fl":"GR","ncumul_tested":"","ncumul_conf":"569","ncumul_hosp":"59","ncumul_ICU":"","ncumul_vent":"","ncumul_released":"","ncumul_deceased":"23","source":"https://www.gr.ch/coronavirus"}
-]
+features: [
+{
+    attributes: {
+            Eingangs_Datum: 1582675200000,
+            Anzahl_Fälle_total__kumuliert_: 2,
+            Neue_Faelle: 2,
+            Neue_aktive_Fälle: 2,
+            Anzahl_aktive_Fälle_total: 2,
+            Anzahl_Personen_in_Isolation: 0,
+            Anzahl_Personen_in_Quarantäne: 0,
+            Verstorbene: 0,
+            Verstorbene__kumuliert_: 0,
+            Neue_Hospitalisierungen: 0,
+            Hospitalisiert_Total: 0,
+            Neu_Pflege: 0,
+            Hospitalisiert_Pflege: 0,
+            Neu_IPS: 0,
+            Hospialisiert_IPS: 0,
+            Neu_IPS_beatmet: 0,
+            Hospitalisiert_IPS_beatmet: 0,
+            FID: 1
+    }
+},
+{
+    attributes: {
+            Eingangs_Datum: 1582761600000,
+            Anzahl_Fälle_total__kumuliert_: 2,
+            Neue_Faelle: 0,
+            Neue_aktive_Fälle: 0,
+            Anzahl_aktive_Fälle_total: 2,
+            Anzahl_Personen_in_Isolation: 0,
+            Anzahl_Personen_in_Quarantäne: 0,
+            Verstorbene: 0,
+            Verstorbene__kumuliert_: 0,
+            Neue_Hospitalisierungen: 0,
+            Hospitalisiert_Total: 0,
+            Neu_Pflege: 0,
+            Hospitalisiert_Pflege: 0,
+            Neu_IPS: 0,
+            Hospialisiert_IPS: 0,
+            Neu_IPS_beatmet: 0,
+            Hospitalisiert_IPS_beatmet: 0,
+            FID: 2
+    }
+},
 """
 
-json_data = json.loads(d)
-
-# Sort by date, just in case. ISO 8601 is used, so we can just sort using strings.
-json_data.sort(key=lambda x: x['date'])
+assert 'features' in data, "JSON did not contain `features` key"
 
 is_first = True
-for row in json_data:
+for feature in data['features']:
+    row = feature['attributes']
     if not is_first:
         print('-' * 10)
     is_first = False
 
     dd = sc.DayData(canton='GR', url=json_url)
-    if row['time']:
-        dd.datetime = f"{row['date']}T{row['time']}"
-    else:
-        dd.datetime = row['date']
-    dd.tested = row['ncumul_tested']
-    dd.cases = row['ncumul_conf']
-    dd.hospitalized = row['ncumul_hosp']
-    dd.icu = row['ncumul_ICU']
-    dd.vent = row['ncumul_vent']
-    dd.recovered = row['ncumul_released']
-    dd.deaths = row['ncumul_deceased']
+    dd.datetime = datetime.datetime.fromtimestamp(row['Eingangs_Datum'] / 1000).date().isoformat()
+    dd.cases = row['Anzahl_Fälle_total__kumuliert_']
+    dd.hospitalized = row['Hospitalisiert_Total']
+    dd.icu = row['Hospialisiert_IPS']
+    dd.vent = row['Hospitalisiert_IPS_beatmet']
+    # Neue_Hospotalisierungen does currently not match our definition of new_hosp
+    # GR provides this calculated field as the difference between
+    # hospitalized from yesterday and today
+    #dd.new_hosp = row['Neue_Hospitalisierungen']
+    dd.deaths = row['Verstorbene__kumuliert_']
+    dd.isolated = row['Anzahl_Personen_in_Isolation']
+    dd.quarantined = row['Anzahl_Personen_in_Quarantäne']
     print(dd)
