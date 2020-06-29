de83ed606bce778c76173b04063520c3263476a2
Dominik Gehl <gehl@fastmail.fm>
2020-06-29 21:52:07 +0200

    adding Vent for ZH1

--- scrapers/scrape_matrix.py
+++ scrapers/scrape_matrix.py
@@ -36,7 +36,7 @@ matrix = {
     'VD': ['Confirmed cases', 'Deaths', 'Hospitalized', 'ICU'],
     'VS': ['Deaths', 'Hospitalized', 'ICU', 'Vent'],
     'ZG': ['Confirmed cases', 'Deaths', 'Released', 'Hospitalized', 'ICU'],
-    'ZH': ['Confirmed cases', 'Deaths', 'Hospitalized'],
+    'ZH': ['Confirmed cases', 'Deaths', 'Hospitalized', 'Vent'],
     'FL': ['Confirmed cases', 'Deaths'],
 }
 
