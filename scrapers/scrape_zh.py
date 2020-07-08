6b75393174dffe8391243c8b3cfad520100edd8b
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-07-09 00:31:00 +0200

    Remove dependency of IDs

--- scrapers/scrape_zh.py
+++ scrapers/scrape_zh.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import csv
+import re
 from io import StringIO
+from bs4 import BeautifulSoup
 import scrape_common as sc
 
 url = "https://www.zh.ch/de/gesundheit/coronavirus.html#-1310230111"
@@ -10,16 +12,34 @@ url = "https://www.zh.ch/de/gesundheit/coronavirus.html#-1310230111"
 dd_iso_q = sc.DayData(canton='ZH', url=url)
 d = sc.download(url, silent=True)
 
-# 2020-04-07
+# 2020-07-08
 """
-<h3>Die Situation im Kanton Zürich am Dienstag, 7. April 2020, 15.00 Uhr</h3>
+<div class="mdl-richtext ">
+<h2 class="atm-heading" id="-1310230111" tabindex="-1">Gesundheitliche Lage</h2>
+<p class="atm-paragraph">Personen mit Wohnsitz im Kanton Zürich<br> </p>
+<h4 class="atm-heading" id="-718243468">23</h4>
+<p class="atm-paragraph">neue positive Fälle in den letzten 24 Stunden</p>
+<h4 class="atm-heading" id="-718243501">11</h4>
+<p class="atm-paragraph">in Spitalbehandlung</p>
+<h4 class="atm-heading" id="808114848">3</h4>
+<p class="atm-paragraph">davon mit künstlicher Beatmung</p>
+<h4 class="atm-heading" id="-790711940">131</h4>
+<p class="atm-paragraph">Total Verstorbene seit Pandemiebeginn (78 in Alters- und Pflegeheimen, 51 im Spital, 2 Zuhause)</p>
+<h4 class="atm-heading" id="-790711785">181</h4>
+<p class="atm-paragraph">in Isolation</p>
+<h4 class="atm-heading" id="-790704311">914</h4>
+<p class="atm-paragraph">in Quarantäne &nbsp;</p>
+<h4 class="atm-heading" id="1798737408">16'362</h4>
+<p class="atm-paragraph">liessen sich vom 29. Juni bis 5. Juli 2020 testen, davon waren 196 positiv</p>
+<p class="atm-paragraph">Diese Zahlen wurden publiziert am 8. Juli 2020 um 14:30 Uhr. Die Zahlen zu Isolation und Quarantäne werden jeweils dienstags und donnerstags aktualisiert.</p>
+</div>
 """
 
-date_time_info = sc.find('publiziert am (.+) Uhr', d)
-date_time_info = date_time_info.replace(' um', ',') + ' Uhr'
+soup = BeautifulSoup(d, 'html.parser')
+date_time_info = sc.find('publiziert am (.+Uhr)', d)
 dd_iso_q.datetime = date_time_info
-dd_iso_q.isolated = sc.find(r'"-790711785">(\d+)</h4>', d)
-dd_iso_q.quarantined = sc.find(r'-790704311">(\d+)</h4>', d)
+dd_iso_q.isolated = soup.find(string=re.compile(r'in Isolation')).find_previous('h4').text
+dd_iso_q.quarantined = soup.find(string=re.compile(r'in Quarantäne')).find_previous('h4').text
 
 print(dd_iso_q)
 
