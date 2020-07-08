f8cbc061a777fa873cdd3cc86063fc7f53b47a99
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-07-08 23:39:06 +0200

    Do not expect hospitalization data for JU

--- scrapers/scrape_ju.py
+++ scrapers/scrape_ju.py
@@ -4,9 +4,7 @@
 from bs4 import BeautifulSoup
 import scrape_common as sc
 
-print('JU')
 d = sc.download('https://www.jura.ch/fr/Autorites/Coronavirus/Chiffres-H-JU/Evolution-des-cas-COVID-19-dans-le-Jura.html', silent=True)
-sc.timestamp()
 
 soup = BeautifulSoup(d, 'html.parser')
 box = soup.find('li', class_="ico-xlsx")
@@ -16,7 +14,6 @@ if not xls_url.startswith('http'):
     xls_url = f'https://www.jura.ch{xls_url}'
 
 xls = sc.xlsdownload(xls_url, silent=True)
-sc.timestamp()
 
 rows = sc.parse_xls(xls, header_row=0)
 is_first = True
@@ -25,11 +22,10 @@ for i, row in enumerate(rows):
         print('-' * 10)
     is_first = False
 
-    print('JU')
-    sc.timestamp()
-    print('Downloading:', xls_url)
-    print('Date and time:', row['Date'].date().isoformat())
-    print('Confirmed cases:', row['Cumul des cas confimés'])
-    print('Hospitalized:', row['Nb cas actuellement hospitalisés'])
-    print('ICU:', row['Nb cas actuellement en SI'])
-    print('Deaths:', sum(r['Nombre de nouveaux décès'] for r in rows[:i+1]))
+    dd = sc.DayData(canton='JU', url=xls_url)
+    dd.datetime = row['Date'].date().isoformat()
+    dd.cases = row['Cumul des cas confimés']
+    dd.hospitalized = row.get('Nb cas actuellement hospitalisés')
+    dd.icu = row.get('Nb cas actuellement en SI')
+    dd.deaths = sum(r['Nombre de nouveaux décès'] for r in rows[:i+1])
+    print(dd)
