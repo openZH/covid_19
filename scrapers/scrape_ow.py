9b19e36ddcde433863e1d2a109fc4776d73b2077
Dominik Gehl <gehl@fastmail.fm>
2020-07-20 19:49:16 +0200

    OW xlsx parser improvements

--- scrapers/scrape_ow.py
+++ scrapers/scrape_ow.py
@@ -8,7 +8,7 @@ import scrape_common as sc
 d = sc.download('https://www.ow.ch/de/kanton/publired/publikationen/?action=info&pubid=20318',
                 encoding='windows-1252', silent=True)
 soup = BeautifulSoup(d, 'html.parser')
-xls_url = soup.find(href=re.compile("\.XLSX$")).get('href')
+xls_url = soup.find(href=re.compile("\.xlsx$", flags=re.IGNORECASE)).get('href')
 assert xls_url, "URL is empty"
 
 for row in soup.find_all('tr'):
@@ -17,21 +17,21 @@ for row in soup.find_all('tr'):
         file_date = cells[1].string
 
 xls = sc.xlsdownload(xls_url, silent=True)
-rows = sc.parse_xls(xls, header_row=0)
+rows = sc.parse_xls(xls, header_row=4)
 is_first = True
 for row in rows:
     if isinstance(row['A'], datetime.datetime):
         dd = sc.DayData(canton='OW', url=xls_url)
         dd.datetime = row['A']
         data_found = False
-        if isinstance(row['B'], int):
-            dd.cases = row['B']
+        if isinstance(row['Infizierte Personen'], int):
+            dd.cases = row['Infizierte Personen']
             data_found = True
-        if isinstance(row['D'], int):
-            dd.hospitalized = row['D']
+        if isinstance(row['Hospitalisierte Personen'], int):
+            dd.hospitalized = row['Hospitalisierte Personen']
             data_found = True
-        if isinstance(row['E'], int):
-            dd.deaths = row['E']
+        if isinstance(row['Gestorbene Personen'], int):
+            dd.deaths = row['Gestorbene Personen']
             data_found = True
         if data_found:
             if not is_first:
