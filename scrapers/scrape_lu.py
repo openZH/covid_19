9fa0915bbb3a03c7f12f4e9908179a79af83b75b
Stefan Oderbolz <oderbolz@gmail.com>
2020-07-24 13:19:52 +0200

    Use `string` attribute

--- scrapers/scrape_lu.py
+++ scrapers/scrape_lu.py
@@ -92,8 +92,8 @@ for table in soup.find(string=re.compile(r'Informationen\s*des\s*Kantons')).find
         header_str = "".join([str(x) for x in cells[0].contents])
 
         value_str = cells[1].find('p') or cells[1]
-        value_str = value_str.replace('*', '')
-        value = int(value_str.string)
+        value_str = value_str.string.replace('*', '')
+        value = int(value_str)
         if re.search('Bestätigte Fälle|Positiv getestet', header_str):
             dd.datetime = case_date_str
             dd.cases = value
