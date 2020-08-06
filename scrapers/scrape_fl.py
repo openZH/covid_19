d30a2b57d5ee4a34df02c1df9cf1f9b097f42e7a
Dominik Gehl <gehl@fastmail.fm>
2020-08-06 10:43:40 +0200

    adding assertion to alert of PDF change

--- scrapers/scrape_fl.py
+++ scrapers/scrape_fl.py
@@ -34,9 +34,11 @@ else:
 # get the data from PDF file containing full history
 history_url = 'https://www.llv.li/files/ag/aktuelle-fallzahlen.pdf'
 d = sc.pdfdownload(history_url, layout=True, silent=True)
+data_in_history_found = False
 for row in d.splitlines():
     m = re.search(r'^(?:Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag),\s+(.+\d{4})\s+(\d+)\s+(\d+)\s+(\d+)$', row)
     if m:
+        data_in_history_found = True
         dd_full_list = sc.DayData(canton='FL', url=history_url)
         dd_full_list.datetime = m[1]
         dd_full_list.cases = m[2]
@@ -45,3 +47,4 @@ for row in d.splitlines():
         print('-' * 10)
         print(dd_full_list)
 
+assert data_in_history_found, f"Unable to retrieve data from {history_url}"
