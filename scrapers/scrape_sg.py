74755f0b78613353dbabc21c45f5f45b3d3bd965
Dominik Gehl <gehl@fastmail.fm>
2020-08-17 14:40:15 +0200

    SG: adapting to new page design

--- scrapers/scrape_sg.py
+++ scrapers/scrape_sg.py
@@ -22,20 +22,20 @@ hosp_date = hosp_table.find_next(string=re.compile("Stand")).string
 dd_hosp.datetime = sc.find(r'Stand:?\s*(.+[0-9]{4})', hosp_date)
 
 rows = hosp_table.find_all('tr')
-assert len(rows) == 2, f"Number of rows changed, {len(rows)} != 2"
 
 headers = rows[0].find_all('td') or rows[0].find_all('th')
-assert len(headers) == 3, f"Number of header columns changed, {len(headers)} != 3"
-assert headers[0].text.strip() == "Total Covid-19-Patienten"
-assert headers[1].text == "Patienten auf der Intensivstation"
-assert headers[2].text == "Davon Patienten mit Beatmung"
+assert len(headers) == 2, f"Number of header columns changed, {len(headers)} != 2"
+assert headers[0].text.strip() == "Covid-19 Patienten in St.Galler Spitälern"
+assert headers[1].text == "Anzahl"
 
-cells = rows[1].find_all('td')
-assert len(cells) == 3, f"Number of columns changed, {len(cells)} != 3"
-
-dd_hosp.hospitalized = cells[0].text
-dd_hosp.icu = cells[1].text
-dd_hosp.vent = cells[2].text
+for i in range(1, len(rows)):
+    cells = rows[i].find_all('td')
+    if cells[0].text.strip() == 'Total Covid-19 Patienten':
+        dd_hosp.hospitalized = cells[1].text
+    elif cells[0].text.strip() == 'davon auf Intensivstation ohne Beatmung':
+        dd_hosp.icu = cells[1].text
+    elif cells[0].text.strip() == 'davon auf Intensivstation mit Beatmung':
+        dd_hosp.vent = cells[1].text
 
 print(dd_hosp)
 
@@ -57,12 +57,12 @@ assert headers[1].text == "Anzahl"
 
 case_cells = rows[1].find_all('td')
 assert len(case_cells) == 3, f"Number of columns changed, {len(case_cells)} != 3"
-assert case_cells[0].text == 'Laborbestätigte Fälle (kummuliert)'
+assert case_cells[0].text == 'Laborbestätigte Fälle (kumuliert)'
 dd_cases.cases = case_cells[1].string
 
 death_cells = rows[2].find_all('td')
 assert len(death_cells) == 3, f"Number of columns changed, {len(death_cells)} != 3"
-assert death_cells[0].text == 'Verstorbene (kummuliert)'
+assert death_cells[0].text == 'Verstorbene (kumuliert)'
 dd_cases.deaths = death_cells[1].string
 
 print(dd_cases)
