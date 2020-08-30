ce779c0ccb48c50347e3dccc9d5541a97bf52c75
Markus Meier <maekke@gentoo.org>
2020-08-30 14:58:07 +0200

    VS: get isolated and quarantined from weekly report

--- scrapers/scrape_vs.py
+++ scrapers/scrape_vs.py
@@ -2,13 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import re
+from bs4 import BeautifulSoup
 import scrape_common as sc
+
+# parse weekly data for isolated and quarantined numbers
+base_url = 'https://www.vs.ch'
+stat_url = base_url + '/de/web/coronavirus/statistiques'
+content = sc.download(stat_url, silent=True)
+soup = BeautifulSoup(content, 'html.parser')
+res = soup.find(string=re.compile(r'Synthese COVID19 VS Woche\d+')).find_previous('a')
+weekly_pdf_url = base_url + res.attrs['href']
+content = sc.pdfdownload(weekly_pdf_url.replace(' ', '%20'), silent=True)
+
+dd = sc.DayData(canton='VS', url=weekly_pdf_url)
+dd.datetime = sc.find(r'vom (\d+)\. bis (\d+\.\d+\.20\d{2})', content, group=2)
+dd.isolated = sc.find(r'befanden\ssich\s(\d+)\spositive\sF.lle\snoch\simmer\sin\sIsolation', content)
+dd.quarantined = sc.find(r'Isolation\sund\s(\d+)\sKontakte\sin\sQuarant.ne', content)
+dd.quarantine_riskareatravel = sc.find(r'\s(\d+)\sReisende\sin\sQuarant.ne', content)
+print(dd)
+
 
 xls_url = 'https://raw.githubusercontent.com/statistikZH/covid19_drop/master/Chiffres%20%20COVID-19%20Valais.xlsx'
 main_url = 'https://www.vs.ch/de/web/coronavirus'
 xls = sc.xlsdownload(xls_url, silent=True)
 rows = sc.parse_xls(xls, header_row=1)
-is_first = True
 for i, row in enumerate(rows):
     if not isinstance(row['Date'], datetime.datetime):
         continue
@@ -17,9 +35,7 @@ for i, row in enumerate(rows):
     if row['Nb nouveaux cas positifs'] is None and row["Nb nouvelles admissions à l'hôpital"] is None:
         continue
 
-    if not is_first:
-        print('-' * 10)
-    is_first = False
+    print('-' * 10)
 
     dd = sc.DayData(canton='VS', url=main_url)
     dd.datetime = row['Date'].date().isoformat()
