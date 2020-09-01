df552c7990b319f53369dfbb66c16a2309c34c80
Markus Meier <maekke@gentoo.org>
2020-09-01 13:14:21 +0200

    move quarantined/isolated data output to top
    - undo "matching" logic to combine DayData items

--- scrapers/scrape_vs.py
+++ scrapers/scrape_vs.py
@@ -6,11 +6,31 @@ import re
 from bs4 import BeautifulSoup
 import scrape_common as sc
 
+# parse weekly data for isolated and quarantined numbers
+base_url = 'https://www.vs.ch'
+stat_url = base_url + '/de/web/coronavirus/statistiques'
+content = sc.download(stat_url, silent=True)
+soup = BeautifulSoup(content, 'html.parser')
+res = soup.find(string=re.compile(r'Synthese COVID19 VS Woche\d+')).find_previous('a')
+weekly_pdf_url = base_url + res.attrs['href']
+content = sc.pdfdownload(weekly_pdf_url.replace(' ', '%20'), silent=True)
+
+
+# add isolated/quarantined to the existing DayData item
+week_end_date = sc.find(r'vom (\d+)\. bis (\d+\.\d+\.20\d{2})', content, group=2)
+week_end_date = sc.date_from_text(week_end_date).isoformat()
+
+dd = sc.DayData(canton='VS', url=weekly_pdf_url)
+dd.datetime = week_end_date
+dd.isolated = sc.find(r'befanden\ssich\s(\d+)\spositive\sF.lle\snoch\simmer\sin\sIsolation', content)
+dd.quarantined = sc.find(r'Isolation\sund\s(\d+)\sKontakte\sin\sQuarant.ne', content)
+dd.quarantine_riskareatravel = sc.find(r'\s(\d+)\sReisende\sin\sQuarant.ne', content)
+print(dd)
+
 xls_url = 'https://raw.githubusercontent.com/statistikZH/covid19_drop/master/Chiffres%20%20COVID-19%20Valais.xlsx'
 main_url = 'https://www.vs.ch/de/web/coronavirus'
 xls = sc.xlsdownload(xls_url, silent=True)
 rows = sc.parse_xls(xls, header_row=1)
-data = []
 for i, row in enumerate(rows):
     if not isinstance(row['Date'], datetime.datetime):
         continue
@@ -29,30 +49,6 @@ for i, row in enumerate(rows):
     dd.deaths = row['Cumul décès COVID-19']
     if row['Nb de nouvelles sorties'] is not None:
         dd.recovered = sum(r['Nb de nouvelles sorties'] for r in rows[:i+1])
-    data.append(dd)
 
-# parse weekly data for isolated and quarantined numbers
-base_url = 'https://www.vs.ch'
-stat_url = base_url + '/de/web/coronavirus/statistiques'
-content = sc.download(stat_url, silent=True)
-soup = BeautifulSoup(content, 'html.parser')
-res = soup.find(string=re.compile(r'Synthese COVID19 VS Woche\d+')).find_previous('a')
-weekly_pdf_url = base_url + res.attrs['href']
-content = sc.pdfdownload(weekly_pdf_url.replace(' ', '%20'), silent=True)
-
-# add isolated/quarantined to the existing DayData item
-week_end_date = sc.find(r'vom (\d+)\. bis (\d+\.\d+\.20\d{2})', content, group=2)
-week_end_date = sc.date_from_text(week_end_date).isoformat()
-for dd in data:
-    if dd.datetime == week_end_date:
-        dd.isolated = sc.find(r'befanden\ssich\s(\d+)\spositive\sF.lle\snoch\simmer\sin\sIsolation', content)
-        dd.quarantined = sc.find(r'Isolation\sund\s(\d+)\sKontakte\sin\sQuarant.ne', content)
-        dd.quarantine_riskareatravel = sc.find(r'\s(\d+)\sReisende\sin\sQuarant.ne', content)
-
-# and print all items
-is_first = True
-for dd in data:
-    if not is_first:
-        print('-' * 10)
-    is_first = False
+    print('-' * 10)
     print(dd)
