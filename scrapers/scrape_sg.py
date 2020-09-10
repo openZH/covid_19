882aa93eff727e0a22c011085054f2fd7d2794c8
Dominik Gehl <gehl@fastmail.fm>
2020-09-10 09:40:31 +0200

    parsing for new SG layout

--- scrapers/scrape_sg.py
+++ scrapers/scrape_sg.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
 
 import re
-import datetime
-import sys
 from bs4 import BeautifulSoup
 import scrape_common as sc
 
-url = 'https://www.sg.ch/tools/informationen-coronavirus.html'
+url = 'https://www.sg.ch/ueber-den-kanton-st-gallen/statistik/covid-19.html'
 d = sc.download(url, silent=True)
 d = d.replace('&nbsp;', ' ')
 
 soup = BeautifulSoup(d, 'html.parser')
 
-# hospitalized
+# cases
+url_cases = 'https://stada.sg.ch/covid/BAG_uebersicht.html'
+soup = BeautifulSoup(sc.download(url_cases, silent=True), 'html.parser')
+dd_cases = sc.DayData(canton='SG', url=url_cases)
+cases_table = soup.find('table')
 
-dd_hosp = sc.DayData(canton='SG', url=url)
-hosp_table = soup.find(string=re.compile(r"Spit.*?ler:\s+Covid-19-Patienten\s+im\s+Kanton\s+St\.\s*Gallen")).find_next('table')
-table_text = " ".join(hosp_table.stripped_strings)
+hosp_date = cases_table.find_next(string=re.compile("Stand")).string
+dd_cases.datetime = sc.find(r'Stand:?\s*(.+[0-9]{4})', hosp_date)
+
+rows = cases_table.find_all('tr')
+
+headers = rows[0].find_all('td') or rows[0].find_all('th')
+assert len(headers) == 2, f"Number of header columns changed, {len(headers)} != 2"
+assert headers[1].text.strip() == "Anzahl"
+
+for row in rows:
+    cells = row.find_all('td')
+    if len(cells) == 2:
+        if cells[0].text.strip() == 'Laborbestätigte Fälle kumuliert':
+            dd_cases.cases = cells[1].string
+        elif cells[0].text.strip() == 'Todesfälle kumuliert':
+            dd_cases.deaths = cells[1].string
+
+print(dd_cases)
+print('-' * 10)
+
+# hospitalized
+url_hospitalized = 'https://stada.sg.ch/covid/C19_Faelle_hospitalisiert.html'
+soup = BeautifulSoup(sc.download(url_hospitalized, silent=True), 'html.parser')
+dd_hosp = sc.DayData(canton='SG', url=url_hospitalized)
+hosp_table = soup.find('table')
 
 hosp_date = hosp_table.find_next(string=re.compile("Stand")).string
 dd_hosp.datetime = sc.find(r'Stand:?\s*(.+[0-9]{4})', hosp_date)
@@ -25,16 +49,15 @@ rows = hosp_table.find_all('tr')
 
 headers = rows[0].find_all('td') or rows[0].find_all('th')
 assert len(headers) == 2, f"Number of header columns changed, {len(headers)} != 2"
-assert headers[0].text.strip() == "Covid-19 Patienten in St.Galler Spitälern"
-assert headers[1].text == "Anzahl"
+assert headers[1].text.strip() == "Anzahl"
 
 for i in range(1, len(rows)):
     cells = rows[i].find_all('td')
     if cells[0].text.strip() == 'Total Covid-19 Patienten':
         dd_hosp.hospitalized = cells[1].text
-    elif cells[0].text.strip() == 'davon auf Intensivstation ohne Beatmung':
+    elif cells[0].text.strip() == '...davon auf Intensivstation ohne Beatmung':
         dd_hosp.icu = int(cells[1].text)
-    elif cells[0].text.strip() == 'davon auf Intensivstation mit Beatmung':
+    elif cells[0].text.strip() == '...davon auf Intensivstation mit Beatmung':
         dd_hosp.vent = int(cells[1].text)
 
 if dd_hosp.vent:
@@ -43,40 +66,26 @@ print(dd_hosp)
 
 print('-' * 10)
 
-# cases
+# isolated / quarantined cases
+url_isolated = 'https://stada.sg.ch/covid/ContactTracing.html'
+soup = BeautifulSoup(sc.download(url_isolated, silent=True), 'html.parser')
+dd_isolated = sc.DayData(canton='SG', url=url_isolated)
+isolated_table = soup.find('table')
 
-dd_cases = sc.DayData(canton='SG', url=url)
-cases_table = soup.find(string=re.compile(r"Aktuelle\s+Lage\s+im\s+Kanton\s+St\.\s*Gallen")).find_next('table')
-table_text = " ".join(cases_table.stripped_strings)
-dd_cases.datetime = sc.find(r'Datenstand:\s+([0-9]+\.\s*[0-9]{2}\.\s*[0-9]{4})', table_text)
+isolated_date = isolated_table.find_next(string=re.compile("Stand")).string
+dd_isolated.datetime = sc.find(r'Stand:?\s*(.+[0-9]{4})', isolated_date)
 
-rows = cases_table.find_all('tr')
-assert len(rows) == 3, f"Number of rows changed, {len(rows)} != 3"
+rows = isolated_table.find_all('tr')
 
 headers = rows[0].find_all('td') or rows[0].find_all('th')
-assert len(headers) == 3, f"Number of header columns changed, {len(headers)} != 3"
-assert headers[1].text == "Anzahl"
+assert len(headers) == 2, f"Number of header columns changed, {len(headers)} != 2"
+assert headers[1].text.strip() == "Anzahl"
 
-case_cells = rows[1].find_all('td')
-assert len(case_cells) == 3, f"Number of columns changed, {len(case_cells)} != 3"
-assert case_cells[0].text == 'Laborbestätigte Fälle (kumuliert)'
-dd_cases.cases = case_cells[1].string
+for i in range(1, len(rows)):
+    cells = rows[i].find_all('td')
+    if cells[0].text.strip() == 'Positiv Getestete im Tracing / in Quarantäne':
+        dd_isolated.isolated = cells[1].text
+    elif cells[0].text.strip() == 'Kontaktpersonen im Tracing / in Quarantäne':
+        dd_isolated.quarantined = int(cells[1].text)
 
-death_cells = rows[2].find_all('td')
-assert len(death_cells) == 3, f"Number of columns changed, {len(death_cells)} != 3"
-assert death_cells[0].text == 'Verstorbene (kumuliert)'
-dd_cases.deaths = death_cells[1].string
-
-print(dd_cases)
-print('-' * 10)
-
-# isolated / quarantined cases
-
-isolation_table = soup.find(string=re.compile(r"Contact Tracing: Anzahl der betreuten Personen")).find_next('table')
-
-dd_isolation = sc.DayData(canton='SG', url=url)
-dd_isolation.datetime = sc.find('Stand ([0-9]{2}.[0-9]{2}.[0-9]{4} [0-9]{2}:[0-9]{2})h', isolation_table.text)
-dd_isolation.isolated = sc.find(r'^(\d+)', isolation_table.find(string=re.compile(r'(Indexf.lle|Positiv Getestete) im Tracing / in Isolation')).find_next('td').find_next('td').text)
-dd_isolation.quarantined = sc.find(r'^(\d+)', isolation_table.find(string=re.compile(r'Kontaktpersonen im Tracing / in Quarant.ne')).find_next('td').find_next('td').text)
-
-print(dd_isolation)
+print(dd_isolated)
