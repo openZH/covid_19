ca31553b7445f40f5d5972626d0c2728f91c1669
Markus Meier <maekke@gentoo.org>
2020-09-15 17:40:52 +0200

    add district ids

--- scrapers/scrape_sg_districts.py
+++ scrapers/scrape_sg_districts.py
@@ -15,6 +15,17 @@ inhabitants = {
     'Wil': 77018,
 }
 
+district_ids = {
+    'St.Gallen': 1721,
+    'Rorschach': 1722,
+    'Rheintal': 1723,
+    'Werdenberg': 1724,
+    'Sarganserland': 1725,
+    'See-Gaster': 1726,
+    'Toggenburg': 1727,
+    'Wil': 1728,
+}
+
 url = 'https://www.sg.ch/ueber-den-kanton-st-gallen/statistik/covid-19/_jcr_content/Par/sgch_downloadlist/DownloadListPar/sgch_download.ocFile/KantonSG_C19-Faelle_download.csv'
 d = sc.download(url, silent=True)
 
@@ -31,6 +42,7 @@ for row in reader:
         dd = sc.DistrictData(canton='SG', district=key)
         dd.url = url
         dd.week = week
+        dd.district_id = district_ids[key]
         dd.new_cases = row['Wahlkreis ' + key]
         dd.total_cases = row['Wahlkreis ' + key + ' (kumuliert)']
         dd.population = value
