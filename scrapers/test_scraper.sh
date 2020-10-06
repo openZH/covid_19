aa3e8eff839ab54d732e731cb748575b6dd697fb
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-10-06 14:17:11 +0200

    Improve test scraper script
    - run the usual `run_scraper.sh` script
    - check the scraper output using `validate_scraper_output.sh`

--- scrapers/test_scraper.sh
+++ scrapers/test_scraper.sh
@@ -11,20 +11,46 @@ function cleanup {
 trap "cleanup" EXIT
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-
+NEWLINE=$'\n'
 
 echo "Run all scrapers..."
 
+exit_code=0
+errors=''
 for scrape_script in $DIR/scrape_??.py
 do
     if [ -f $scrape_script -a -x $scrape_script ]
     then
         name=`basename $scrape_script`
+        canton=${name:7:2}
+        export SCRAPER_KEY=${canton^^}
         echo ""
-        echo "Running $name..."
+        echo "Running ${SCRAPER_KEY} scraper..."
         echo "=========================================="
-        $scrape_script | $DIR/parse_scrape_output.py
+
+        set +e
+        $DIR/run_scraper.sh
+        ret=$?
+        if [ $ret -ne 0 ]
+        then
+            echo "ERROR: ${scrape_script} failed with exit code $ret. continue." >&2
+            errors=$"${errors}${NEWLINE}ERROR: ${scrape_script} failed with exit code $ret"
+            exit_code=1
+        fi
+        $DIR/validate_scraper_output.sh
+        ret=$?
+        if [ $ret -ne 0 ]
+        then
+            echo "ERROR: Validation for ${SCRAPER_KEY} failed with exit code $ret. continue." >&2
+            errors=$"${errors}${NEWLINE}ERROR: Validation for ${SCRAPER_KEY} failed with exit code $ret"
+            exit_code=1
+        fi
+        set -e
+
         echo "=========================================="
         echo ""
     fi
 done
+
+echo "$errors"
+exit $exit_code
