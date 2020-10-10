3b577f0a65e6b09b305b1c8b7fcf6071ccdc340a
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-10-10 12:56:14 +0200

    Add html table scraper for JU

--- scrapers/scrape_ju.py
+++ scrapers/scrape_ju.py
@@ -2,36 +2,68 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import re
 from bs4 import BeautifulSoup
 import scrape_common as sc
 
-d = sc.download('https://www.jura.ch/fr/Autorites/Coronavirus/Chiffres-H-JU/Evolution-des-cas-COVID-19-dans-le-Jura.html', silent=True)
+url = 'https://www.jura.ch/fr/Autorites/Coronavirus/Chiffres-H-JU/Evolution-des-cas-COVID-19-dans-le-Jura.html'
+d = sc.download(url, silent=True)
+d = d.replace('&nbsp;', ' ')
 
 soup = BeautifulSoup(d, 'html.parser')
-box = soup.find(class_="ico-xlsx")
-assert box, "No xlsx in the box"
-xls_url = box.find('a').get('href')
-assert xls_url, "URL is empty"
-if not xls_url.startswith('http'):
-    xls_url = f'https://www.jura.ch{xls_url}'
-
-xls = sc.xlsdownload(xls_url, silent=True)
-
-rows = sc.parse_xls(xls, header_row=0)
 is_first = True
-for i, row in enumerate(rows):
-    if not isinstance(row['Date'], datetime.datetime):
-        continue
+box = soup.find(class_="ico-xlsx")
+if box:
+    xls_url = box.find('a').get('href')
+    assert xls_url, "URL is empty"
+    if not xls_url.startswith('http'):
+        xls_url = f'https://www.jura.ch{xls_url}'
 
-    if not is_first:
-        print('-' * 10)
-    is_first = False
+    xls = sc.xlsdownload(xls_url, silent=True)
 
-    dd = sc.DayData(canton='JU', url=xls_url)
-    dd.datetime = row['Date'].date().isoformat()
-    dd.cases = row['Cumul des cas confimés']
-    dd.hospitalized = row.get('Nb cas actuellement hospitalisés')
-    dd.icu = row.get('Nb cas actuellement en SI')
-    if sc.represents_int(row.get('Nombre de nouveaux décès')):
-        dd.deaths = sum(r['Nombre de nouveaux décès'] for r in rows[:i+1])
-    print(dd)
+    rows = sc.parse_xls(xls, header_row=0)
+    for i, row in enumerate(rows):
+        if not isinstance(row['Date'], datetime.datetime):
+            continue
+
+        if not is_first:
+            print('-' * 10)
+        is_first = False
+
+        dd = sc.DayData(canton='JU', url=xls_url)
+        dd.datetime = row['Date'].date().isoformat()
+        dd.cases = row['Cumul des cas confimés']
+        dd.hospitalized = row.get('Nb cas actuellement hospitalisés')
+        dd.icu = row.get('Nb cas actuellement en SI')
+        if sc.represents_int(row.get('Nombre de nouveaux décès')):
+            dd.deaths = sum(r['Nombre de nouveaux décès'] for r in rows[:i+1])
+        print(dd)
+
+data_table = soup.find('caption', string=re.compile(r'Evolution du nombre de cas.*Jura')).find_parent('table')
+if data_table:
+    headers = [" ".join(cell.stripped_strings) for cell in data_table.find('tr').find_all(['td', 'th'])]
+    assert len(headers) == 5, f"Number of headers changed: {len(headers)} != 5"
+    rows = []
+    for row in data_table.find_all('tr'):
+        data = {}
+        for col_num, cell in enumerate(row.find_all(['th', 'td'])):
+            if cell.string:
+                data[headers[col_num]] = cell.string.strip()
+        rows.append(data)
+
+    if rows:
+        for i, row in enumerate(rows[1:]):
+            if not is_first:
+                print('-' * 10)
+            is_first = False
+
+            if not row.get('Date'):
+                continue
+
+            dd = sc.DayData(canton='JU', url=url)
+            dd.datetime = row.get('Date')
+            dd.cases = row.get('Cumul des cas confirmés')
+            dd.hospitalized = row.get('Nombre de cas actuellement hospitalisés')
+            dd.icu = row.get('Nombre de cas actuellement en soins intensifs')
+            dd.deaths = sum(int(r.get('Nombre de nouveaux décès', 0)) for r in rows[:i+1] if r.get('Nombre de nouveaux décès'))
+            print(dd)
