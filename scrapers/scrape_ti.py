6250467696de745e49dba6d0b0e282a1e60d0e33
Markus Meier <maekke@gentoo.org>
2020-10-23 23:21:54 +0200

    TI: parse PDF containing isolated and quarantined case numbers

--- scrapers/scrape_ti.py
+++ scrapers/scrape_ti.py
@@ -5,10 +5,25 @@ from bs4 import BeautifulSoup
 import re
 import scrape_common as sc
 
-# get xlsx URL from covid19 page of TI
+# get pdf and xlsx URL from covid19 page of TI
 main_url = 'https://www4.ti.ch/dss/dsp/covid19/home/'
 d = sc.download(main_url, silent=True)
 soup = BeautifulSoup(d, 'html.parser')
+
+pdf_url = soup.find('a', string=re.compile(r'Dati stato.*')).get('href')
+pdf_url = f'https://www4.ti.ch/{pdf_url}'
+pdf_content = sc.pdfdownload(pdf_url, silent=True, raw=True)
+
+dd = sc.DayData(canton='TI', url=pdf_url)
+dd.datetime = sc.find(r'Stato (\d+\.\d+\.20\d{2})', pdf_content)
+dd.isolated = sc.find(r'(\d+)\sPersone\sin\sisolamento', pdf_content)
+dd.quarantined = sc.find(r'(\d+)\sPersone\sin\squarantena', pdf_content)
+is_first = True
+if dd:
+    print(dd)
+    is_first = False
+
+
 xls_url = soup.find(href=re.compile("\.xlsx$")).get('href')
 assert xls_url, "URL is empty"
 
@@ -17,7 +32,6 @@ if not xls_url.startswith('http'):
 
 xls = sc.xlsdownload(xls_url, silent=True)
 rows = sc.parse_xls(xls, header_row=0)
-is_first = True
 for row in rows:
     if not is_first:
         print('-' * 10)
