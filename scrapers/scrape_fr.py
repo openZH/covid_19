ab66953aca153e3bc106998242f9f78f7bc771fd
Markus Meier <maekke@gentoo.org>
2020-11-02 20:12:40 +0100

    FR: move xls fetching to common file and function

--- scrapers/scrape_fr.py
+++ scrapers/scrape_fr.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import re
 import datetime
 import sys
-from bs4 import BeautifulSoup
 import scrape_common as sc
+from scrape_fr_common import get_fr_xls
 
-d = sc.download('https://www.fr.ch/sante/covid-19/coronavirus-statistiques-evolution-de-la-situation-dans-le-canton', silent=True)
-
-soup = BeautifulSoup(d, 'html.parser')
-xls_url = soup.find(href=re.compile("\.xlsx$")).get('href')
-assert xls_url, "URL is empty"
-if not xls_url.startswith('http'):
-    xls_url = f'https://www.fr.ch{xls_url}'
-
-xls = sc.xlsdownload(xls_url, silent=True)
+xls_url, xls = get_fr_xls()
 rows = sc.parse_xls(xls, header_row=0)
 is_first = True
 
