11d09a9a6d676fb1dd2aac1b9d246058f790f56a
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-11-03 11:23:24 +0100

    Remove xls parsing for JU

--- scrapers/scrape_ju.py
+++ scrapers/scrape_ju.py
@@ -9,36 +9,9 @@ import scrape_common as sc
 url = 'https://www.jura.ch/fr/Autorites/Coronavirus/Chiffres-H-JU/Evolution-des-cas-COVID-19-dans-le-Jura.html'
 d = sc.download(url, silent=True)
 d = d.replace('&nbsp;', ' ')
-
 soup = BeautifulSoup(d, 'html.parser')
+
 is_first = True
-box = soup.find(class_="ico-xlsx")
-if box:
-    xls_url = box.find('a').get('href')
-    assert xls_url, "URL is empty"
-    if not xls_url.startswith('http'):
-        xls_url = f'https://www.jura.ch{xls_url}'
-
-    xls = sc.xlsdownload(xls_url, silent=True)
-
-    rows = sc.parse_xls(xls, header_row=0)
-    for i, row in enumerate(rows):
-        if not isinstance(row['Date'], datetime.datetime):
-            continue
-
-        if not is_first:
-            print('-' * 10)
-        is_first = False
-
-        dd = sc.DayData(canton='JU', url=xls_url)
-        dd.datetime = row['Date'].date().isoformat()
-        dd.cases = row['Cumul des cas confimés']
-        dd.hospitalized = row.get('Nb cas actuellement hospitalisés')
-        dd.icu = row.get('Nb cas actuellement en SI')
-        if sc.represents_int(row.get('Nombre de nouveaux décès')):
-            dd.deaths = sum(r['Nombre de nouveaux décès'] for r in rows[:i+1])
-        print(dd)
-
 data_table = soup.find('caption', string=re.compile(r'Evolution du nombre de cas.*Jura')).find_parent('table')
 if data_table:
     headers = [" ".join(cell.stripped_strings) for cell in data_table.find('tr').find_all(['td', 'th'])]
