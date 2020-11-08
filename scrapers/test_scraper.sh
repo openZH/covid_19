41223a94c4ae5f1bedef6f6f103f8008f1ad856c
Markus Meier <maekke@gentoo.org>
2020-11-08 11:39:57 +0100

    wire up tests scraper in test_scraper.sh

--- scrapers/test_scraper.sh
+++ scrapers/test_scraper.sh
@@ -79,6 +79,32 @@ do
     fi
 done
 
+for scrape_script in $DIR/scrape_??_tests.py
+do
+    if [ -f $scrape_script -a -x $scrape_script ]
+    then
+        name=`basename $scrape_script`
+        canton=${name:7:2}
+        export SCRAPER_KEY=${canton^^}
+        echo ""
+        echo "Running ${SCRAPER_KEY} tests scraper..."
+        echo "=========================================="
+
+        set +e
+        $DIR/run_tests_scraper.sh
+        ret=$?
+        if [ $ret -ne 0 ]
+        then
+            echo "ERROR: ${scrape_script} failed with exit code $ret. continue." >&2
+            errors=$"${errors}${NEWLINE}ERROR: ${scrape_script} failed with exit code $ret"
+            exit_code=1
+        fi
+        set -e
+
+        echo "=========================================="
+        echo ""
+    fi
+done
 
 
 echo "$errors"
