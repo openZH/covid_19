e7af6380bf5610c1c704aac17b447b0faed04352
Stefan Oderbolz <oderbolz@gmail.com>
2020-11-08 09:07:40 +0100

    Fix 2020-11-07 typo of BE

--- scrapers/scrape_be.py
+++ scrapers/scrape_be.py
@@ -31,6 +31,8 @@ for t in soup.find_all('table', {'summary': 'Laufend aktualisierte Zahlen zu den
 
             if headers[col_num] == 'Datum':
                 date_string = "".join(list(cell.stripped_strings)[0:-1])
+                if date_string == '07.10.2020':
+                    date_string = '07.11.2020'
                 time_string = list(cell.stripped_strings)[-1]
                 dd.datetime=  f'{date_string} {time_string}'
             elif headers[col_num] == 'Fälle positiv':
