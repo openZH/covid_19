990e92c74f0ca0a2637b9858f3e004b7629897f3
Markus Meier <maekke@gentoo.org>
2020-11-11 20:32:47 +0100

    add a function to get all weekly PDFs
    update get_latest_weekly to use this one and return the most recent element

--- scrapers/scrape_so_common.py
+++ scrapers/scrape_so_common.py
@@ -11,10 +11,19 @@ def strip_value(value):
 
 
 def get_latest_weekly_pdf_url():
+    return get_all_weekly_pdf_urls()[0]
+
+
+def get_all_weekly_pdf_urls():
     base_url = 'https://corona.so.ch'
     url = f'{base_url}/bevoelkerung/daten/woechentlicher-situationsbericht/'
     d = sc.download(url, silent=True)
     soup = BeautifulSoup(d, 'html.parser')
-    pdf_url = soup.find(href=re.compile(r'\.pdf$')).get('href')
-    pdf_url = f'{base_url}{pdf_url}'
-    return pdf_url
+    links = soup.find_all(href=re.compile(r'\.pdf$'))
+    result = []
+    for link in links:
+        file_ref = link.get('href')
+        url = f'{base_url}{file_ref}'
+        if url not in result:
+            result.append(url)
+    return result
