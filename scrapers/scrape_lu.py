233f7f8002ae32c2f0f14b5d9e02cecdbb19909c
Markus Meier <maekke@gentoo.org>
2020-11-11 19:04:03 +0100

    update LU scraper to use different csv files as historized sources

--- scrapers/scrape_lu.py
+++ scrapers/scrape_lu.py
@@ -1,9 +1,71 @@
 #!/usr/bin/env python3
 
+import csv
+from io import StringIO
 import re
 import datetime
 from bs4 import BeautifulSoup
 import scrape_common as sc
+
+
+def fix_lu_date(date):
+    res = re.match(r'(20\d{2})/(\d+)/(\d+)', date)
+    assert res, 'date could not be matched!'
+    date = datetime.date(int(res[1]), int(res[2]) + 1, int(res[3]))
+    return date.isoformat()
+
+
+hosp_url = 'https://www.lustat.ch/analysen/gesundheit/corona-reporting/hospitalisationen'
+hosp_csv = 'https://www.lustat.ch/files_ftp/daten/covid/cov_hospitalisationen.csv'
+
+is_first = True
+data = sc.download(hosp_csv, silent=True, encoding='utf-8-sig')
+reader = csv.DictReader(StringIO(data), delimiter=';')
+for row in reader:
+    dd = sc.DayData(canton='LU', url=hosp_url)
+    dd.datetime = fix_lu_date(row['utcdatum'])
+    dd.hospitalized = row['current_hosp']
+    dd.vent = row['current_vent']
+    if dd:
+        if not is_first:
+            print('-' * 10)
+        is_first = False
+        print(dd)
+
+
+cases_url = 'https://www.lustat.ch/analysen/gesundheit/corona-reporting/entwicklungen-seit-maerz-2020'
+cases_csv = 'https://www.lustat.ch/files_ftp/daten/covid/cov_faelle_g2.csv'
+
+data = sc.download(cases_csv, silent=True, encoding='utf-8-sig')
+reader = csv.DictReader(StringIO(data), delimiter=';')
+for row in reader:
+    dd = sc.DayData(canton='LU', url=cases_url)
+    dd.datetime = fix_lu_date(row['utcdatum'])
+    dd.cases = row['cumnew_conf']
+    if dd:
+        if not is_first:
+            print('-' * 10)
+        is_first = False
+        print(dd)
+
+
+deceased_csv = 'https://www.lustat.ch/files_ftp/daten/covid/cov_todesfaelle.csv'
+
+is_first = True
+data = sc.download(deceased_csv, silent=True, encoding='utf-8-sig')
+reader = csv.DictReader(StringIO(data), delimiter=';')
+deaths = 0
+for row in reader:
+    dd = sc.DayData(canton='LU', url=cases_url)
+    dd.datetime = fix_lu_date(row['utcdatum'])
+    deaths += int(row['new_deceased'])
+    dd.deaths = deaths
+    if dd:
+        if not is_first:
+            print('-' * 10)
+        is_first = False
+        print(dd)
+
 
 start_url = 'https://gesundheit.lu.ch/themen/Humanmedizin/Infektionskrankheiten/Informationen_Coronavirus'
 d = sc.download(start_url, silent=True)
@@ -19,7 +81,6 @@ hosp_date_str = sc.find(r'Hospitalisierungen.*\(Stand:\s*(.+?)\,', d)
 isolated_date_str = sc.find(r'Isolation.*\(Stand:\s*(.+?)\,', d)
 
 soup = BeautifulSoup(d, 'html.parser')
-is_first = True
 tables = soup.find_all('table')
 assert tables, f"Couldn't find tables on {cases_url}"
 for table in tables:
