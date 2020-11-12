f2b0e6396ad468c174f29f8fa38d4805baced0c3
Markus Meier <maekke@gentoo.org>
2020-11-12 20:20:15 +0100

    scrape isolated, quarantined and travel quarantined from new page
    and remove the old page scraping

--- scrapers/scrape_lu.py
+++ scrapers/scrape_lu.py
@@ -67,56 +67,30 @@ for row in reader:
         print(dd)
 
 
-start_url = 'https://gesundheit.lu.ch/themen/Humanmedizin/Infektionskrankheiten/Informationen_Coronavirus'
-d = sc.download(start_url, silent=True)
+url = 'https://www.lustat.ch/analysen/gesundheit/corona-reporting'
+d = sc.download(url, silent=True)
 soup = BeautifulSoup(d, 'html.parser')
-cases_url = soup.find(string=re.compile(r'Aktuelle Zahlen.*Kanton\s+Luzern')).find_previous('a')['href']
-if not cases_url.startswith('http'):
-    cases_url = f"https://gesundheit.lu.ch{cases_url}"
-d = sc.download(cases_url, silent=True)
-d = d.replace('&nbsp;', ' ')
+header = soup.find('h2', string=re.compile(r'Aktuellste Zahlen'))
+paragraph = header.find_next('p')
+date = sc.find(r'Am (\d+\.\d+\.20\d{2})', paragraph.text)
+table = header.find_next('table')
 
-case_date_str = sc.find(r'Fallzahlen\s*im\s*Kanton\s*Luzern.*\(Stand:\s*(.+?)\,', d)
-hosp_date_str = sc.find(r'Hospitalisierungen.*\(Stand:\s*(.+?)\,', d)
-isolated_date_str = sc.find(r'Isolation.*\(Stand:\s*(.+?)\,', d)
-
-soup = BeautifulSoup(d, 'html.parser')
-tables = soup.find_all('table')
-assert tables, f"Couldn't find tables on {cases_url}"
-for table in tables:
-    if not is_first:
-        print('-' * 10)
-    is_first = False
-
-    dd = sc.DayData(canton='LU', url=cases_url)
-    for row in table.find_all('tr'):
-        cells = row.find_all('td')
-        assert len(cells) == 2, "Number of columns changed, not 2"
-
-        header_str = "".join([str(x) for x in cells[0].contents])
-
-        value_str = cells[1].find('p') or cells[1]
-        if value_str is None:
-            continue
-        value_str = value_str.string.replace('*', '')
-        value = int(value_str)
-        if re.search('Bestätigte Fälle|Positiv getestet', header_str):
-            dd.datetime = case_date_str
-            dd.cases = value
-        if re.search('Todesfälle', header_str):
-            dd.datetime = case_date_str
-            dd.deaths = value
-        if re.search('Hospitalisiert', header_str):
-            dd.datetime = hosp_date_str
-            dd.hospitalized = value
-        if re.search('Intensivpflege', header_str):
-            dd.datetime = hosp_date_str
-            dd.icu = value
-        if re.search('Personen in Isolation', header_str):
-            dd.datetime = isolated_date_str
+for row in table.find_all('tr'):
+    columns = row.find_all('td')
+    if len(columns) == 2:
+        dd = sc.DayData(canton='LU', url=url)
+        dd.datetime = date
+        category = columns[0].text
+        value = columns[1].text.replace(' ', '')
+        if category == 'In Isolation':
             dd.isolated = value
-        if re.search('Personen in Quarantäne', header_str):
-            dd.datetime = isolated_date_str
+        if category == 'In Quarantäne':
             dd.quarantined = value
+        if category == 'Reiserückkehrer in Quarantäne':
+            dd.quarantine_riskareatravel = value
 
-    print(dd)
+        if dd:
+            if not is_first:
+                print('-' * 10)
+            is_first = False
+            print(dd)
