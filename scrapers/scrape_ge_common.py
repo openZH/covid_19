75e6f6ee75a53a7f603eb4ed99d503aa3cd09cc0
Markus Meier <maekke@gentoo.org>
2020-11-14 22:50:17 +0100

    add get_ge_weekly_pdf_urls to get all PDF urls

--- scrapers/scrape_ge_common.py
+++ scrapers/scrape_ge_common.py
@@ -7,10 +7,19 @@ import scrape_common as sc
 
 
 def get_latest_ge_weekly_pdf_url():
+    return get_ge_weekly_pdf_urls()[0]
+
+
+def get_ge_weekly_pdf_urls():
     d = sc.download('https://www.ge.ch/document/covid-19-bilan-epidemiologique-hebdomadaire', silent=True)
     soup = BeautifulSoup(d, 'html.parser')
-    pdf_url = soup.find(title=re.compile("\.pdf$")).get('href')
-    assert pdf_url, "pdf URL is empty"
-    if not pdf_url.startswith('http'):
-        pdf_url = f'https://www.ge.ch{pdf_url}'
-    return pdf_url
+    links = soup.find_all('a', title=re.compile(r"\.pdf$"))
+    result = []
+    for link in links:
+        pdf_url = link.get('href')
+        assert pdf_url, "pdf URL is empty"
+        if not pdf_url.startswith('http'):
+            pdf_url = f'https://www.ge.ch{pdf_url}'
+        if pdf_url not in result:
+            result.append(pdf_url)
+    return result
