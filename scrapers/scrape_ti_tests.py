6b3ba5df708356d41c726b5344f9dcf166c61a5e
Markus Meier <maekke@gentoo.org>
2020-11-14 06:15:53 +0100

    add TI tests scraper

---
+++ scrapers/scrape_ti_tests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import re
+import scrape_common as sc
+
+main_url = 'https://www4.ti.ch/dss/dsp/covid19/home/'
+d = sc.download(main_url, silent=True)
+soup = BeautifulSoup(d, 'html.parser')
+
+td = sc.TestData(canton='TI', url=main_url)
+
+container = soup.find('h2', string=re.compile(r'Test PCR \(tampone\)')).find_next('div')
+for item in container.find_all('div'):
+    divs = item.find_all('div')
+    if len(divs) == 3:
+        if divs[2].string:
+            date = sc.find(r'.*?(\d+\.\d+\.\d{2})', divs[2].string)
+            date = sc.date_from_text(date)
+            td.start_date = date.isoformat()
+            td.end_date = date.isoformat()
+        if sc.find(r'^(Totale test).*', divs[1].string):
+            td.total_tests = divs[0].string
+        if sc.find(r'^(% test).*', divs[1].string):
+            td.positivity_rate = divs[0].string
+
+if td:
+    assert td.start_date and td.end_date, 'failed to extract date'
+    print(td)
