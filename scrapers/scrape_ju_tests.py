bcc95156fc1df8395fa44e721645d08cb0406703
Markus Meier <maekke@gentoo.org>
2020-11-16 20:42:08 +0100

    add JU tests scraper

---
+++ scrapers/scrape_ju_tests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+from bs4 import BeautifulSoup
+import scrape_common as sc
+
+base_url = 'https://www.jura.ch'
+url = f'{base_url}/fr/Autorites/Coronavirus/Chiffres-H-JU/Evolution-des-cas-COVID-19-dans-le-Jura.html'
+d = sc.download(url, silent=True)
+d = d.replace('&nbsp;', ' ')
+soup = BeautifulSoup(d, 'html.parser')
+
+pdf_url = soup.find('a', title=re.compile(r'.*PDF.*')).get('href')
+if not pdf_url.startswith('http'):
+    pdf_url = f'{base_url}{pdf_url}'
+
+pdf = sc.download_content(pdf_url, silent=True)
+
+td = sc.TestData(canton='JU', url=pdf_url)
+
+content = sc.pdftotext(pdf, page=1)
+td.week = sc.find(r'Situation semaine épidémiologique (\d+)', content)
+td.year = sc.find(r'Du \d+.* (\d{4})', content)
+
+content = sc.pdftotext(pdf, page=2)
+td.total_tests = sc.find(r'Nombre de tests\s+(\d+)', content)
+res = re.match(r'.*Nombre de tests positifs .*\s+(\d+)\s+\((\d+\.?\d?)%\)', content, re.DOTALL | re.MULTILINE)
+assert res, 'failed to find number of positive tests and positivity rate'
+td.positive_tests = res[1]
+td.positivity_rate = res[2]
+
+print(td)
