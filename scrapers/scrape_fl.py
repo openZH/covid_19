2a9cdfcef91d9821d01e750f3fdfb684c7b646c8
Stefan Oderbolz <oderbolz@gmail.com>
2020-11-18 21:06:39 +0100

    Replace separator instead of adapting the regex

--- scrapers/scrape_fl.py
+++ scrapers/scrape_fl.py
@@ -15,12 +15,13 @@ if pdf_url:
     d = sc.pdfdownload(pdf_url, raw=True, silent=True)
     # extract case numbers reported for previous days
     d = d.replace(u'\xa0', u' ')
+    d = d.replace("'", "")
 
     # data from the most recent press release
     dd = sc.DayData(canton='FL', url=pdf_url)
     dd.datetime = sc.find(r'Situationsbericht vom (.*? 20\d{2})', d)
 
-    dd.cases = sc.find(r"insgesamt\s+([0-9\']+)\s+laborbestätigte\s+Fälle", d)
+    dd.cases = sc.find(r"insgesamt\s+([0-9]+)\s+laborbestätigte\s+Fälle", d)
     m = re.search(r'Bisher\s+trat(en)?\s+(\S+)\s+(Todesfall|Todesfälle)', d, flags=re.I)
     if m:
         dd.deaths = sc.int_or_word(m[2])
