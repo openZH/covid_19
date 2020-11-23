7e37b3ecb762b70bf0d82036c95a8573a395a257
Markus Meier <maekke@gentoo.org>
2020-11-23 22:48:10 +0100

    BL tests: scrape all available bulletins

--- scrapers/scrape_bl_common.py
+++ scrapers/scrape_bl_common.py
@@ -7,9 +7,16 @@ import scrape_common as sc
 
 
 def get_latest_bl_bulletin_url():
+    return get_all_bl_bulletin_urls()[0]
+
+
+def get_all_bl_bulletin_urls():
     news_url = 'https://www.baselland.ch/politik-und-behorden/direktionen/volkswirtschafts-und-gesundheitsdirektion/amt-fur-gesundheit/medizinische-dienste/kantonsarztlicher-dienst/aktuelles/medienmitteilungen-1'
     news_content = sc.download(news_url, silent=True)
     soup = BeautifulSoup(news_content, 'html.parser')
 
-    bulletin_url = soup.find('td', string=re.compile(r'Coronavirus: Wochenbulletin.*')).find_previous('a').get('href')
-    return bulletin_url
+    bulletins = soup.find_all('a', href=re.compile(r'.*/coronavirus-wochenbulletin.*'))
+    bulletin_urls = []
+    for bulletin in bulletins:
+        bulletin_urls.append(bulletin.get('href'))
+    return bulletin_urls
--- scrapers/scrape_bl_tests.py
+++ scrapers/scrape_bl_tests.py
@@ -8,24 +8,24 @@ import scrape_bl_common as sbc
 from datetime import timedelta
 
 
-bulletin_url = sbc.get_latest_bl_bulletin_url()
-bulletin_content = sc.download(bulletin_url, silent=True)
-soup = BeautifulSoup(bulletin_content, 'html.parser')
-content = soup.find('strong', string=re.compile(r'Per heute .*')).string
-# strip unwanted characters
-content = content.encode("ascii", errors="ignore").decode()
+bulletin_urls = sbc.get_all_bl_bulletin_urls()
+for bulletin_url in bulletin_urls:
+    bulletin_content = sc.download(bulletin_url, silent=True)
+    soup = BeautifulSoup(bulletin_content, 'html.parser')
+    content = soup.find('p', string=re.compile(r'Per heute .*')).string
+    # strip unwanted characters
+    content = content.encode("ascii", errors="ignore").decode()
 
-date = sc.find(r'Per heute \w+, (\d+\. \w+ 20\d{2})', content)
-date = sc.date_from_text(date)
-# previous week
-date = date - timedelta(days=7)
+    date = sc.find(r'Per heute \w+, (\d+\. \w+ 20\d{2})', content)
+    date = sc.date_from_text(date)
+    # previous week
+    date = date - timedelta(days=7)
 
-td = sc.TestData(canton='BL', url=bulletin_url)
-td.week = date.isocalendar()[1]
-td.year = date.year
-
-td.total_tests = sc.find(r'In der Vorwoche wurden (\d+) PCR-Tests', content)
-td.positivity_rate = sc.find(r'von diesen waren (\d+\.?\d?) Prozent positiv', content)
-
-if td:
-    print(td)
+    td = sc.TestData(canton='BL', url=bulletin_url)
+    td.week = date.isocalendar()[1]
+    td.year = date.year
+    td.total_tests = sc.find(r'In der Vorwoche wurden (\d+) PCR-Tests', content)
+    td.positivity_rate = sc.find(r'von diesen waren (\d+\.?,?\d?) Prozent positiv', content)
+    if td.total_tests and td.positivity_rate:
+        td.positivity_rate = td.positivity_rate.replace(',', '.')
+        print(td)
