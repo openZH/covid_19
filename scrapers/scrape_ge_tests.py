8e176e03f0951f497c764d9b370206c2cd5bb168
Markus Meier <maekke@gentoo.org>
2020-12-08 18:33:28 +0100

    GE: loop through all pages and extract test data

--- scrapers/scrape_ge_tests.py
+++ scrapers/scrape_ge_tests.py
@@ -14,22 +14,24 @@ for pdf_url in pdf_urls:
     week_number = sc.find(r'Situation semaine (\d+)', content)
     year = sc.find(r'au \d+(\w+)? \w+ (\d{4})', content, group=2)
 
-    # data is on page 6 from W48 2020
-    for page in [4, 6]:
+    pages = int(sc.pdfinfo(pdf))
+    for page in range(3, pages):
         content = sc.pdftotext(pdf, page=page)
         # remove ' separator to simplify pattern matching
         content = re.sub(r'(\d)\'(\d)', r'\1\2', content)
 
-        weekly_tests = sc.find(r'avec\s(\d+)\stests\s(effectués\s?)?(contre|\.)', content)
-        res = re.match(r'.*taux\sde\spositivité.*\s\(?(\d+\.?\d?)%\)?\s(en|durant).*\d+\.?\d?%', content, re.MULTILINE | re.DOTALL)
-        positivity_rate = None
-        if res:
-            positivity_rate = res[1]
+        if sc.find(r'(Dynamique et tendances épidémiologiques)', content):
+            weekly_tests = sc.find(r'avec\s(\d+)\stests\s(effectués\s?)?(contre|\.)', content)
+            res = re.match(r'.*taux\sde\spositivité.*\s\(?(\d+\.?\d?)%\)?\s(en|durant).*\d+\.?\d?%', content, re.MULTILINE | re.DOTALL)
+            positivity_rate = None
+            if res:
+                positivity_rate = res[1]
 
-        if weekly_tests and positivity_rate:
-            td = sc.TestData(canton='GE', url=pdf_url)
-            td.week = week_number
-            td.year = year
-            td.total_tests = weekly_tests
-            td.positivity_rate = positivity_rate
-            print(td)
+            if weekly_tests and positivity_rate:
+                td = sc.TestData(canton='GE', url=pdf_url)
+                td.week = week_number
+                td.year = year
+                td.total_tests = weekly_tests
+                td.positivity_rate = positivity_rate
+                print(td)
+                break
