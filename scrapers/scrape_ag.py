b5e0fb637f58074ed6c0c9206b1b91d931cb5dad
Markus Meier <maekke@gentoo.org>
2020-12-29 14:35:18 +0100

    fix columns for deaths and recovered for AG

--- scrapers/scrape_ag.py
+++ scrapers/scrape_ag.py
@@ -58,8 +58,8 @@ for row in rows:
         dd.hospitalized = int(non_icu) + int(icu) + int(icf)
         dd.icu = icu
         dd.icf = icf
-    dd.deaths = row['Gesamtzahl16']
-    dd.recovered = row['Gesamtzahl20']
+    dd.deaths = row['Gesamtzahl17']
+    dd.recovered = row['Gesamtzahl21']
 
     if dd:
         if not is_first:
