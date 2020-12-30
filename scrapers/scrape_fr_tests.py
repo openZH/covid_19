c1dc60653998c7bad7b64acf032ed8e185a9b3e5
Markus Meier <maekke@gentoo.org>
2020-12-30 09:18:05 +0100

    skip empty rows in FR tests scraper

--- scrapers/scrape_fr_tests.py
+++ scrapers/scrape_fr_tests.py
@@ -8,8 +8,12 @@ xls_url, xls = get_fr_xls()
 rows = sc.parse_xls(xls, header_row=0, sheet_name='tests', enable_float=True)
 
 for row in rows:
+    week = row['semaine /Woche']
+    if not week:
+        continue
+
     td = sc.TestData(canton='FR', url=xls_url)
-    td.week = row['semaine /Woche']
+    td.week = week
     td.year = '2020'
     tot_ag = int(row['Tests AG'])
     tot_pcr = int(row['Tests PCR'])
