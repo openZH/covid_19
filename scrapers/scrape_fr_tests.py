eb29b82193e342d54860f2da0f8455b6c43c7727
Markus Meier <maekke@gentoo.org>
2021-01-07 21:14:37 +0100

    FR: manually determine the year for now
    since it is not provided by the sheet.

--- scrapers/scrape_fr_tests.py
+++ scrapers/scrape_fr_tests.py
@@ -7,14 +7,19 @@ from scrape_fr_common import get_fr_xls
 xls_url, xls = get_fr_xls()
 rows = sc.parse_xls(xls, header_row=0, sheet_name='tests', enable_float=True)
 
+year = '2020'
+
 for row in rows:
     week = row['semaine /Woche']
     if not week:
         continue
 
+    if week == 1:
+        year = '2021'
+
     td = sc.TestData(canton='FR', url=xls_url)
     td.week = week
-    td.year = '2020'
+    td.year = year
     tot_ag = int(row['Tests AG'])
     tot_pcr = int(row['Tests PCR'])
     td.total_tests = tot_ag + tot_pcr
