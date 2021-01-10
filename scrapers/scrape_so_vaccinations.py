0e7e2e33e723b87de52bc53098b7ecab0c3054c8
Markus Meier <maekke@gentoo.org>
2021-01-10 10:34:44 +0100

    add SO vaccinations scraper

---
+++ scrapers/scrape_so_vaccinations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import re
+from bs4 import BeautifulSoup
+import scrape_common as sc
+
+url = 'https://corona.so.ch/bevoelkerung/daten/impfstatistik/'
+d = sc.download(url, silent=True)
+soup = BeautifulSoup(d, 'html.parser')
+
+title = soup.find('h3', string=re.compile(r'^Stand \d+\.')).text
+date = sc.find(r'Stand (\d+\.\d+\.20\d{2}),', title)
+date = sc.date_from_text(date)
+
+element = soup.find('td', string=re.compile('Anzahl Impfungen \(kumuliert\)'))
+element = element.find_next('td')
+
+vd = sc.VaccinationData(canton='SO', url=url)
+vd.date = date.isoformat()
+vd.total_vaccinations = element.text.replace("'", "")
+if vd:
+    print(vd)
