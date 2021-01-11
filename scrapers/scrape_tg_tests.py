d99bff98e7939300720b9ecc85fd7fa8d526cb7a
Markus Meier <maekke@gentoo.org>
2021-01-11 20:08:26 +0100

    add year logic to TG tests scraper to properly handle 2020/21

--- scrapers/scrape_tg_tests.py
+++ scrapers/scrape_tg_tests.py
@@ -23,10 +23,14 @@ assert res, f'failed to extract positivtiy rate, got {res}'
 positivity_rate = res[1].split(',')
 
 assert len(weeks) == len(negative_tests) == len(positive_tests) == len(positivity_rate), f'Expected same length for weeks {len(weeks)}, neg. tests {len(negative_tests)}, pos. tests {len(positive_tests)}, pos. rate {len(positivity_rate)}'
+
+year = '2020'
 for week, neg, pos, rate in zip(weeks, negative_tests, positive_tests, positivity_rate):
     td = sc.TestData(canton='TG', url=url)
     td.week = sc.find(r'KW (\d+)', week)
-    td.year = '2020'
+    if int(td.week) == 1:
+        year = '2021'
+    td.year = year
     td.positive_tests = int(pos)
     td.negative_tests = int(neg)
     td.positivity_rate = float(rate)
