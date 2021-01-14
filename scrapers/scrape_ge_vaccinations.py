ecda1bcfa4f2b8288a605326c62fc729ec5ac73e
Markus Meier <maekke@gentoo.org>
2021-01-14 21:34:22 +0100

    update GE vaccination scraper to new website

--- scrapers/scrape_ge_vaccinations.py
+++ scrapers/scrape_ge_vaccinations.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 
 import re
+import datetime
 from bs4 import BeautifulSoup
 import scrape_common as sc
 
 url = 'https://www.ge.ch/se-faire-vacciner-contre-covid-19/vaccination-chiffres'
 d = sc.download(url, silent=True)
+d = re.sub(r'(\d+)\'(\d+)', r'\1\2', d)
 soup = BeautifulSoup(d, 'html.parser')
 
-table = soup.find('p', string=re.compile('Nombre de vaccinations')).find_next('table')
+table = soup.find('strong', string=re.compile('VACCINATIONS CONTRE LA COVID-19 PAR SEMAINE')).find_next('table')
 trs = table.find_all('tr')
-assert len(trs) == 3, f'expected 3 rows, but got {len(trs)} ({trs})'
+for tr in trs[1:]:
+    tds = tr.find_all('td')
+    assert len(tds) == 3, f'Expected 3 columns, but got: {tds}'
+    date = sc.find(r'(\d+\.\d+[\.-]\d{4})', tds[0].text)
+    if not date and sc.find(r'(D.cembre 2020)', tds[0].text):
+        # let's use the last day of the year (-6 days, see below)
+        date = '2020-12-25'
+    # week 2 is '11.01-2021'...
+    date = date.replace('-2021', '.2021')
+    date = sc.date_from_text(date)
+    # this is the start date of the week, let's use the last day
+    date = date + datetime.timedelta(days=6)
 
-date_row = trs[0].find_all('td')
-vaccination_row = trs[2].find_all('td')
-assert len(date_row) == len(vaccination_row), f'expected number of elements in dates and vaccinations to be equal! got {len(date_row)} and {len(vaccination_row)}'
-
-# skip the first two columns, the first one is the "title",
-# the second one is December 2020, which cannot really be represented properly.
-for i in range(2, len(date_row)):
     vd = sc.VaccinationData(canton='GE', url=url)
-    date = sc.date_from_text(date_row[i].text)
     vd.date = date.isoformat()
-    vd.total_vaccinations = vaccination_row[i].text
+    vd.total_vaccinations = sc.find(r'^(\d+)\s?', tds[2].text)
     if vd:
         print(vd)
