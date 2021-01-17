b6db4c8a65a581abcb28c2f8fd720343385c371e
Markus Meier <maekke@gentoo.org>
2021-01-17 13:21:26 +0100

    update BE vaccinations scraper to use latest website

--- scrapers/scrape_be_vaccinations.py
+++ scrapers/scrape_be_vaccinations.py
@@ -4,16 +4,16 @@ import re
 from bs4 import BeautifulSoup
 import scrape_common as sc
 
-url = 'https://www.gef.be.ch/gef/de/index/Corona/Corona/corona_impfung_bern.html'
+url = 'https://www.besondere-lage.sites.be.ch/de/start/impfen.html'
 d = sc.download(url, silent=True)
 d = re.sub(r'(\d+)\'(\d+)', r'\1\2', d)
 soup = BeautifulSoup(d, 'html.parser')
 
-table = soup.find('h2', string=re.compile('Durchgef.hrte Impfungen im Kanton Bern')).find_next('table')
+table = soup.find('p', string=re.compile('Durchgef.hrte Impfungen im Kanton Bern')).find_next('table')
 tbody = table.find_all('tbody')[0]
 trs = tbody.find_all('tr')
 
-for tr in trs:
+for tr in trs[1:]:
     tds = tr.find_all('td')
     assert len(tds) == 3, f'expected 3 rows, but got {len(tds)} ({tds})'
 
