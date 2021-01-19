d0a8fc27131cf6d97ff9ecb4908a924006783739
Markus Meier <maekke@gentoo.org>
2021-01-19 21:38:13 +0100

    use week and year for GE vaccination scraper

--- scrapers/scrape_ge_vaccinations.py
+++ scrapers/scrape_ge_vaccinations.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import re
-import datetime
 from bs4 import BeautifulSoup
 import scrape_common as sc
 
@@ -16,23 +15,21 @@ total_vaccinations = 0
 for tr in trs[1:]:
     tds = tr.find_all('td')
     assert len(tds) == 2, f'Expected 2 columns, but got: {tds}'
-    weekly_vaccinations = int(sc.find(r'^(\d+)\s?', tds[1].text))
+    weekly_vaccinations = int(sc.find(r'\s?(\d+)\s?', tds[1].text))
     if sc.find(r'(Cumul des vaccinations)', tds[0].text):
         assert weekly_vaccinations == total_vaccinations, f'expected {weekly_vaccinations}, but got {total_vaccinations}'
         continue
 
     date = sc.find(r'(\d+\.\d+[\.-]\d{4})', tds[0].text)
-    if not date and sc.find(r'(D.cembre 2020)', tds[0].text):
-        # let's use the last day of the year (-6 days, see below)
-        date = '2020-12-25'
-    # week 2 is '11.01-2021'...
-    date = date.replace('-2021', '.2021')
-    date = sc.date_from_text(date)
-    # this is the start date of the week, let's use the last day
-    date = date + datetime.timedelta(days=6)
+    week = sc.find(r'Semaine (\d+)', tds[0].text)
+    year = sc.find(r'\s\d+\.\d+\.(\d{4})', tds[0].text)
+    if not week and sc.find(r'(D.cembre 2020)', tds[0].text):
+        # let's use the last week of the year
+        week = 53
 
     vd = sc.VaccinationData(canton='GE', url=url)
-    vd.date = date.isoformat()
+    vd.week = week
+    vd.year = year
     vd.total_vaccinations = weekly_vaccinations + total_vaccinations
     total_vaccinations = vd.total_vaccinations
     print(vd)
