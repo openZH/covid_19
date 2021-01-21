358736af54b991a39aff247ef4220a47cbfbe775
Markus Meier <maekke@gentoo.org>
2021-01-21 21:28:57 +0100

    update BS vaccination scraper
    - update total column name
    - add first and second dosis values

--- scrapers/scrape_bs_vaccinations.py
+++ scrapers/scrape_bs_vaccinations.py
@@ -13,6 +13,8 @@ for row in reader:
     vd = sc.VaccinationData(canton='BS', url=url)
     vd.start_date = row['Datum']
     vd.end_date = row['Datum']
-    vd.total_vaccinations = row['Total geimpfte Personen ']
+    vd.total_vaccinations = row['Total verabreichte Impfungen']
+    vd.first_doses = row['Total Personen mit erster Dosis']
+    vd.second_doses = row['Total Personen mit zweiter Dosis']
     if vd:
         print(vd)
