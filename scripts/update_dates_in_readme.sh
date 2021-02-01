3fb56b9de8a5a6fd6c23ba917d31b92b3b71ff0c
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2021-02-01 15:26:17 +0100

    Script to update README

---
+++ scripts/update_dates_in_readme.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+today=$(date +%s)
+
+areas="FL AG AI AR BE BL BS FR GE GL GR JU LU NE NW OW SG SH SO SZ TG TI UR VD VS ZG ZH"
+for area in $areas
+do
+    update_date_str=`grep $area $DIR/../COVID19_Fallzahlen_CH_total_v2.csv | tail -n 1 | awk -F, '{print $1}'`
+    update_date=$(date --date="$update_date_str" +%s)
+    echo $today
+    echo $update_date
+    diff=$(($today-$update_date))
+
+    if [ $diff -lt 84000 ]; then
+        color='4d9221'
+    elif [ $diff -lt 144000 ]; then
+        color='b8e186'
+    else
+        color='de77ae'
+    fi
+    echo "${area},${update_date_str},${diff},${color}"
+    sed -i -e "/\[$area\]/s#update on [^|]*|#update on $update_date_str](https://placehold.it/130x50/$color/000000?text=$update_date_str 'Last update on $update_date_str')|#" $DIR/../README.md
+done
