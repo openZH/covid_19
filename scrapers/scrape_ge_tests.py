5c36d3e1bf1840c28e09c5ed7b7965dc53ac2bba
Stefan Oderbolz <oderbolz@gmail.com>
2021-02-10 09:27:00 +0100

    Use wait method of selenium

--- scrapers/scrape_ge_tests.py
+++ scrapers/scrape_ge_tests.py
@@ -5,6 +5,9 @@ import re
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import scrape_common as sc
 import scrape_ge_common as sgc
@@ -21,7 +24,12 @@ elem = driver.find_element_by_link_text('Graphiques')
 elem.click()
 elem = driver.find_element_by_partial_link_text('Tests')
 elem.click()
-xls_url = sgc.get_link_from_element(driver, 'save_plot_nombre_tests_data')
+
+# wait for the element
+wait = WebDriverWait(driver, 10)
+elem = wait.until(EC.element_to_be_clickable((By.ID, 'save_plot_nombre_tests_data')))
+xls_url = elem.get_attribute('href')
+assert xls_url, "Couldn't find XLS url"
 
 xls = sc.xlsdownload(xls_url, silent=True)
 rows = sc.parse_xls(xls, header_row=0, enable_float=True)
