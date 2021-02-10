85def2ff9b1330791a742289ae5229ab78ffc4d9
Stefan Oderbolz <oderbolz@gmail.com>
2021-02-10 09:35:49 +0100

    Remove unused imports

--- scrapers/scrape_ge_tests.py
+++ scrapers/scrape_ge_tests.py
@@ -5,9 +5,6 @@ import re
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 import scrape_common as sc
 import scrape_ge_common as sgc
