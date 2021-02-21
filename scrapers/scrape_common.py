4732aadc36cc4dffc0c3ca308bf94c6d1dd131c1
Stefan Oderbolz <oderbolz@gmail.com>
2021-02-21 11:09:52 +0100

    Add backoff_factor=2 to let retries wait

--- scrapers/scrape_common.py
+++ scrapers/scrape_common.py
@@ -359,6 +359,7 @@ def _download_request(url, silent):
 
     retry_strategy = Retry(
         total=3,
+        backoff_factor=2,
         status_forcelist=[429, 500, 502, 503, 504],
         method_whitelist=["HEAD", "GET", "OPTIONS"]
     )
