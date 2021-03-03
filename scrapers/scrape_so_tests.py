092d00884960305f71ffdd718e75406c66e3a65d
Markus Meier <maekke@gentoo.org>
2021-03-03 19:03:47 +0100

    update SO tests regex for latest PDF

--- scrapers/scrape_so_tests.py
+++ scrapers/scrape_so_tests.py
@@ -26,13 +26,13 @@ for pdf_url in pdf_urls:
 
     res = re.match(r'.*PCR-Tes\s?ts\s+(\d.*\n)?Total\s+\d+\s+\d+\s+(\d+)\s+\d+\.?\d?\s+(\d+)\s', content, re.DOTALL)
     if not res:
-        res = re.match(r'.*Labortes\s?ts\s\(PCR\s-\sund\sS\s?chnelltes\s?ts\s?\)\s+(\d.*\n)?Total\s+\d+\s+\d+\s+(\d+)\s+\d+\.?\d?\s+(\d+)\s', content, re.DOTALL)
+        res = re.match(r'.*Labortes\s?ts\s\(PCR\s-\sund\sS\s?chnelltes\s?ts\s?\)\s+(\d.*\n)?Total\s+\d+\s+\d+\.?\d?\s+(\d+)\s+\d+\.?\d?\s+(\d+)\s', content, re.DOTALL)
     assert res, f'PCR tests for week {week1} or {week2} could not be extracted!'
     total_tests1 = res[2]
     total_tests2 = res[3]
 
     #res = re.match(r'.*Positivit.tsrate\s+\*?\s+\d.*%\s+(\d.*)%\s+(\d.*)%', content, re.DOTALL)
-    res = re.match(r'.*Positivit.tsrate\s+\*+?\s+\d+\.?\d?%\s+(\d+\.?\d?)%\s+(\d+\.?\d?)%', content, re.DOTALL)
+    res = re.match(r'.*Positivit.tsrate\s+\*+?\s+\d+\.?\d?%?\s+(\d+\.?\d?)%?\s+(\d+\.?\d?)%?', content, re.DOTALL)
     assert res, f'Positivity rate for week {week1} or {week2} could not be extracted!'
     pos_rate1 = res[1]
     pos_rate2 = res[2]
