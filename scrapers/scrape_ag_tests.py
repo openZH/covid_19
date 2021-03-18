b6cb4d02e3b32c093a55063922716a6d031068ff
Markus Meier <maekke@gentoo.org>
2021-03-18 17:26:00 +0100

    add support to parse AG tests containing antigen and PCR positivity rate

--- scrapers/scrape_ag_tests.py
+++ scrapers/scrape_ag_tests.py
@@ -12,6 +12,8 @@ rows = sc.parse_xls(xls, sheet_name='1.4 Labortests', header_row=1, enable_float
 for row in rows:
     if not row['Anzahl Tests']:
         continue
+    if row['Anzahl Tests'] == 'Anzahl Tests':
+        break
 
     td = sc.TestData(canton='AG', url=xls_url)
     td.week = int(row['Kalenderwoche'])
@@ -22,5 +24,23 @@ for row in rows:
     td.negative_tests = int(row['Negative Tests'])
     td.total_tests = int(row['Anzahl Tests'])
     td.positivity_rate = float(row['Positivitätsrate'])
+    if td:
+        print(td)
+
+rows = sc.parse_xls(xls, sheet_name='1.4 Labortests', header_row=51, enable_float=True)
+for row in rows:
+    if not row['Anzahl Tests']:
+        continue
+
+    td = sc.TestData(canton='AG', url=xls_url)
+    td.week = int(row['Kalenderwoche'])
+    if td.week == 1:
+        year = '2021'
+    td.year = year
+    td.positive_tests = int(row['Positive Tests'])
+    td.negative_tests = int(row['Negative Tests'])
+    td.total_tests = int(row['Anzahl Tests'])
+    td.pcr_positivity_rate = float(row['Positivitätsrate'])
+    td.ag_positivity_rate = float(row['F'])
     if td:
         print(td)
