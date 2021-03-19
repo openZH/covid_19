8132e18b5df43371524b50d12f1396f77da4458c
Markus Meier <maekke@gentoo.org>
2021-03-19 16:44:34 +0100

    update AG tests scraper with unified table

--- scrapers/scrape_ag_tests.py
+++ scrapers/scrape_ag_tests.py
@@ -4,6 +4,18 @@ import scrape_common as sc
 import scrape_ag_common as sac
 
 
+def get_value_int(value):
+    if value is not None and value != '':
+        return int(value)
+    return None
+
+
+def get_value_float(value):
+    if value is not None and value != '':
+        return float(value)
+    return None
+
+
 xls_url = sac.get_ag_xls_url()
 xls = sc.xlsdownload(xls_url, silent=True)
 
@@ -19,28 +31,11 @@ for row in rows:
     td.week = int(row['Kalenderwoche'])
     if td.week == 1:
         year = '2021'
-    td.year = year
-    td.positive_tests = int(row['Positive Tests'])
-    td.negative_tests = int(row['Negative Tests'])
+    td.positive_tests = get_value_int(row['Positive Tests'])
+    td.negative_tests = get_value_int(row['Negative Tests'])
     td.total_tests = int(row['Anzahl Tests'])
-    td.positivity_rate = float(row['Positivitätsrate'])
-    if td:
-        print(td)
-
-rows = sc.parse_xls(xls, sheet_name='1.4 Labortests', header_row=51, enable_float=True)
-for row in rows:
-    if not row['Anzahl Tests']:
-        continue
-
-    td = sc.TestData(canton='AG', url=xls_url)
-    td.week = int(row['Kalenderwoche'])
-    if td.week == 1:
-        year = '2021'
-    td.year = year
-    td.positive_tests = int(row['Positive Tests'])
-    td.negative_tests = int(row['Negative Tests'])
-    td.total_tests = int(row['Anzahl Tests'])
-    td.pcr_positivity_rate = float(row['Positivitätsrate'])
-    td.ag_positivity_rate = float(row['F'])
+    td.positivity_rate = get_value_float(row['Positivitätsrate'])
+    td.pcr_positivity_rate = get_value_float(row['F'])
+    td.ag_positivity_rate = get_value_float(row['G'])
     if td:
         print(td)
