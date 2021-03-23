3e59cf13b83daa63beeb73402ff91fe0857d801a
Markus Meier <maekke@gentoo.org>
2021-03-23 21:18:29 +0100

    add antigen and PCR test data to BS tests scraper

--- scrapers/scrape_bs_tests.py
+++ scrapers/scrape_bs_tests.py
@@ -5,6 +5,12 @@ from io import StringIO
 import scrape_common as sc
 
 
+def prettify_positivity_rate(positivity_rate):
+    if not positivity_rate:
+        return None
+    return round(10 * float(positivity_rate)) / 10
+
+
 url = 'https://data.bs.ch/explore/dataset/100094/download/?format=csv&timezone=Europe/Berlin&lang=en&use_labels_for_header=true&csv_separator=%3B'
 data = sc.download(url, silent=True)
 
@@ -17,7 +23,19 @@ for row in reader:
     td.negative_tests = row['Negative Tests'] or None
     td.total_tests = row['Total Tests'] or None
     td.positivity_rate = row['Anteil positive Tests in Prozent'] or None
+
+    td.pcr_positive_tests = row['Positive PCR Tests'] or None
+    td.pcr_negative_tests = row['Negative PCR Tests'] or None
+    td.pcr_total_tests = row['Total PCR Tests'] or None
+    td.pcr_positivity_rate = row['Anteil positive PCR Tests in Prozent'] or None
+
+    td.ag_positive_tests = row['Positive Antigen Schnelltests'] or None
+    td.ag_negative_tests = row['Negative Antigen Schnelltests'] or None
+    td.ag_total_tests = row['Total Antigen Schnelltests'] or None
+    td.ag_positivity_rate = row['Anteil positive Antigen Schnelltests in Prozent'] or None
+
     if td:
-        # prettify output a bit
-        td.positivity_rate = round(10 * float(td.positivity_rate)) / 10
+        td.positivity_rate = prettify_positivity_rate(td.positivity_rate)
+        td.pcr_positivity_rate = prettify_positivity_rate(td.pcr_positivity_rate)
+        td.ag_positivity_rate = prettify_positivity_rate(td.ag_positivity_rate)
         print(td)
