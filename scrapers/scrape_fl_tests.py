b50d52b043ddd859fd0e53489a65f9d24b1fa241
Markus Meier <maekke@gentoo.org>
2021-05-18 20:12:43 +0200

    fix row for this week FL tests data

--- scrapers/scrape_fl_tests.py
+++ scrapers/scrape_fl_tests.py
@@ -4,7 +4,7 @@ import scrape_common as sc
 
 url = 'https://www.llv.li/files/as/grafik_covid19_tests_pro_kw.xlsx'
 xls = sc.xlsdownload(url, silent=True)
-rows = sc.parse_xls(xls, header_row=61, sheet_name='gTests_AG')
+rows = sc.parse_xls(xls, header_row=68, sheet_name='gTests_AG')
 year = '2020'
 for row in rows:
     if row['B'] is None:
