9640cf97110d68ace50f8eb214339b983fd841c4
Markus Meier <maekke@gentoo.org>
2021-07-21 21:56:12 +0200

    WIP: scrape powerbi page for JU data

--- scrapers/scrape_ju.py
+++ scrapers/scrape_ju.py
@@ -1,115 +1,100 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import datetime
 import re
 import sys
 import traceback
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-import time
-import pandas as pd
+import requests
 import scrape_common as sc
 
 
-def load_with_selenium(url):
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(options=options)
-
-    driver.get(url)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "columnHeaders")))
-    wait.until(EC.presence_of_element_located((By.XPATH, "//input[contains(@class, 'date-slicer-input')]")))
-    
-    # select the complete date range by setting 2020-02-24 as start date
-    begin = driver.find_element(By.XPATH, "//input[contains(@class, 'date-slicer-input')]")
-    begin.click()
-    begin.send_keys(Keys.CONTROL + "a")
-    begin.send_keys(Keys.DELETE)
-    begin.clear()
-    begin.send_keys("2/24/2020") # 2020-02-24 is the date of the earliest data from JU
-    begin.send_keys(Keys.ENTER)
-    driver.find_element(By.XPATH, "//div[contains(@class, 'slicer-header')]").click()
-    time.sleep(5)
-    return driver
-
-def scrape_page_part(html):
-    table = BeautifulSoup(html, 'html.parser')
-
-    headers = [" ".join(cell.stripped_strings) for cell in table.find(class_='columnHeaders').find_all('div', class_='pivotTableCellWrap')]
-    assert len(headers) == 6, f"Number of headers changed: {len(headers)} != 6"
-    assert headers[0] == 'Date', f"Header changed to {headers[0]}"
-    assert headers[1] == 'Nouveaux cas', f"Header changed to {headers[1]}"
-    assert headers[2] == 'Cumul des cas confirmés', f"Header changed to {headers[2]}"
-    assert headers[3] == 'Cas actuellement hospitalisés', f"Header changed to {headers[3]}"
-    assert headers[4] == 'Cas actuellement en soins intensifs', f"Header changed to {headers[4]}"
-    assert headers[5] == 'Nouveaux décès', f"Header changed to {headers[5]}"
-    
-    columns = table.find(class_='bodyCells').find('div', recursive=False).find('div', recursive=False).findChildren('div', recursive=False)
-    assert len(columns) == 6, f"Number of columns changed: {len(columns)} != 6"
-
-    cols = {}
-    for i, col in enumerate(columns):
-        values = []
-        for cell in col.find_all('div'):
-            values.append(" ".join(cell.stripped_strings).strip())
-        cols[headers[i]] = values
-
-    rows = pd.DataFrame.from_dict(cols).to_dict('records')
-    return rows
-
-
 url = 'https://www.jura.ch/fr/Autorites/Coronavirus/Infos-Actualite/Statistiques-COVID/Evolution-des-cas-COVID-19-dans-le-Jura.html'
-d = sc.download(url, silent=True)
-d = d.replace('&nbsp;', ' ')
-soup = BeautifulSoup(d, 'html.parser')
+
+
+def download_data():
+    json_url = 'https://wabi-west-europe-b-primary-api.analysis.windows.net/public/reports/querydata?synchronous=true'
+
+    req_data = '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"tableau","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"},"Name":"tableau.Date"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Nouveaux cas"},"Name":"Sum(tableau.Nouveaux_cas)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cumul des cas confirmÃ©s"}},"Function":0},"Name":"Sum(tableau.Cumul des cas)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cas actuellement hospitalisÃ©s"}},"Function":0},"Name":"Sum(tableau.Hospitalisations)"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Nouveaux dÃ©cÃ¨s"},"Name":"Sum(tableau.Nouveaux dÃ©cÃ¨s)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cas actuellement en soins intensifs"}},"Function":0},"Name":"Sum(tableau.Cas actuallement en soins intensifs)"}],"Where":[{"Condition":{"Comparison":{"ComparisonKind":2,"Left":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"}},"Right":{"Literal":{"Value":"datetime\'2020-02-29T00:00:00\'"}}}}}],"OrderBy":[{"Direction":2,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1,2,3,4,5]}]},"DataReduction":{"DataVolume":3,"Primary":{"Window":{"Count":500}}},"Version":1},"ExecutionMetricsKind":1}}]},"QueryId":"","ApplicationContext":{"DatasetId":"5268a522-8bbd-41a0-82c9-c842f7b82739","Sources":[{"ReportId":"f13b6b6f-82a9-40ad-a7ba-64c3c6e7565e","VisualId":"c8ae720f712bb1124e47"}]}}],"cancelQueries":[],"modelId":1961083}'
+
+    http = requests.Session()
+    headers = {
+        'ActivityId': '13794e30-5afe-4904-a5d4-298294a8ea19',
+        'Origin': 'https://app.powerbi.com',
+        'Referer': 'https://app.powerbi.com/',
+        'RequestId': '978d9a2f-a5c2-d2ae-52af-952ce6951d8c',
+        'User-Agent': 'Mozilla Firefox Mozilla/5.0; openZH covid_19 at github',
+        'X-PowerBI-ResourceKey': 'ae57abf2-47e2-420e-a5c0-69b0ef3380ab',
+    }
+
+    r = http.post(json_url, data=req_data, headers=headers, timeout=10)
+    r.raise_for_status()
+    return r.json()
+
+
+json = download_data()
+json = json['results']
+json = json[0]
+json = json['result']
+json = json['data']
+json = json['dsr']
+json = json['DS']
+json = json[0]
+json = json['PH']
+json = json[0]
+json = json['DM0']
 
 is_first = True
-iframe = soup.find(string=re.compile(r'Evolution du nombre de cas.*Jura')).find_next('iframe')
-if iframe and iframe['src']:
-    rows = []
-    last_rows = {}
-    
-    # scroll through Power BI table
-    # each time the table is scrolled, a new slice of data is loaded in the table
-    i = 0
-    while True:
-        try:
-            driver = load_with_selenium(iframe['src'])
-            scroll = driver.find_elements(By.XPATH, "//div[contains(@class, 'scroll-bar-part-bar')]")[1]
-            action = ActionChains(driver)
-            action.drag_and_drop_by_offset(scroll, 0, i * 20).perform()
+for item in json:
+    data = item['C']
+    r = None
+    if 'R' in item:
+        r = item['R']
+    if len(data) > 0:
+        dd = sc.DayData(canton='JU', url=url)
+        date = int(data[0]) / 1000
+        dd.datetime = datetime.datetime.utcfromtimestamp(date).isoformat()
+        if not r:
+            dd.cases = data[3]
+            dd.hospitalized = data[4]
+            dd.icu = data[5]
+        elif r == 4:
+            dd.cases = data[2]
+            dd.hospitalized = data[3]
+            dd.icu = data[4]
+        elif r == 6:
+            dd.cases = data[2]
+            dd.hospitalized = data[3]
+        elif r == 16:
+            dd.cases = data[3]
+            dd.icu = data[4]
+        elif r == 20:
+            dd.cases = data[2]
+            dd.hospitalized = data[3]
+        elif r == 36:
+            dd.cases = data[2]
+            dd.hospitalized = data[3]
+        elif r == 44:
+            dd.hospitalized = data[2]
+        elif r == 46:
+            dd.hospitalized = data[1]
+        elif r == 38:
+            dd.cases = data[1]
+            dd.hospitalized = data[2]
+        elif r == 52:
+            dd.cases = data[2]
+        elif r == 54:
+            dd.cases = data[1]
+        elif r == 60:
+            pass
+        elif r == 62:
+            pass
+        else:
+            print(f'>>> unexpected r: {r}')
 
-            current_rows = scrape_page_part(driver.page_source)
-            driver.quit()
-            # if we don't get new data after scrolling, we are at the end
-            if current_rows == last_rows:
-                break
-            rows.extend(current_rows)
-            last_rows = current_rows
-            time.sleep(2)
-        except Exception as e:
-            print("Error: %s" % e, file=sys.stderr)
-            print(traceback.format_exc(), file=sys.stderr)
-            break
-        i += 1
-
-    if rows:
-        for i, row in enumerate(rows):
+        if dd:
             if not is_first:
                 print('-' * 10)
             is_first = False
-
-            dd = sc.DayData(canton='JU', url=url)
-            dd.datetime = row.get('Date', '')
-            dd.cases = row.get('Cumul des cas confirmés')
-            dd.hospitalized = row.get('Cas actuellement hospitalisés')
-            dd.icu = row.get('Cas actuellement en soins intensifs')
-            dd.deaths = sum(int(str(r.get('Nouveaux décès', 0))) for r in rows[i:] if r.get('Nouveaux décès'))
             print(dd)
