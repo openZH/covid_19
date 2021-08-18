667cd32c0d95634bdf0cfc72eb03a0303e628452
Markus Meier <maekke@gentoo.org>
2021-08-18 09:49:58 +0200

    cover all cases for r
    inverse date order to start in 2020 and sum up deaths

--- scrapers/scrape_ju.py
+++ scrapers/scrape_ju.py
@@ -16,7 +16,7 @@ url = 'https://www.jura.ch/fr/Autorites/Coronavirus/Infos-Actualite/Statistiques
 def download_data():
     json_url = 'https://wabi-west-europe-b-primary-api.analysis.windows.net/public/reports/querydata?synchronous=true'
 
-    req_data = '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"tableau","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"},"Name":"tableau.Date"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Nouveaux cas"},"Name":"Sum(tableau.Nouveaux_cas)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cumul des cas confirmÃ©s"}},"Function":0},"Name":"Sum(tableau.Cumul des cas)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cas actuellement hospitalisÃ©s"}},"Function":0},"Name":"Sum(tableau.Hospitalisations)"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Nouveaux dÃ©cÃ¨s"},"Name":"Sum(tableau.Nouveaux dÃ©cÃ¨s)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cas actuellement en soins intensifs"}},"Function":0},"Name":"Sum(tableau.Cas actuallement en soins intensifs)"}],"Where":[{"Condition":{"Comparison":{"ComparisonKind":2,"Left":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"}},"Right":{"Literal":{"Value":"datetime\'2020-02-29T00:00:00\'"}}}}}],"OrderBy":[{"Direction":2,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1,2,3,4,5]}]},"DataReduction":{"DataVolume":3,"Primary":{"Window":{"Count":500}}},"Version":1},"ExecutionMetricsKind":1}}]},"QueryId":"","ApplicationContext":{"DatasetId":"5268a522-8bbd-41a0-82c9-c842f7b82739","Sources":[{"ReportId":"f13b6b6f-82a9-40ad-a7ba-64c3c6e7565e","VisualId":"c8ae720f712bb1124e47"}]}}],"cancelQueries":[],"modelId":1961083}'
+    req_data = '{"version":"1.0.0","queries":[{"Query":{"Commands":[{"SemanticQueryDataShapeCommand":{"Query":{"Version":2,"From":[{"Name":"t","Entity":"tableau","Type":0}],"Select":[{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"},"Name":"tableau.Date"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Nouveaux cas"},"Name":"Sum(tableau.Nouveaux_cas)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cumul des cas confirmÃ©s"}},"Function":0},"Name":"Sum(tableau.Cumul des cas)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cas actuellement hospitalisÃ©s"}},"Function":0},"Name":"Sum(tableau.Hospitalisations)"},{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Nouveaux dÃ©cÃ¨s"},"Name":"Sum(tableau.Nouveaux dÃ©cÃ¨s)"},{"Aggregation":{"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Cas actuellement en soins intensifs"}},"Function":0},"Name":"Sum(tableau.Cas actuallement en soins intensifs)"}],"Where":[{"Condition":{"Comparison":{"ComparisonKind":2,"Left":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"}},"Right":{"Literal":{"Value":"datetime\'2020-02-29T00:00:00\'"}}}}}],"OrderBy":[{"Direction":1,"Expression":{"Column":{"Expression":{"SourceRef":{"Source":"t"}},"Property":"Date"}}}]},"Binding":{"Primary":{"Groupings":[{"Projections":[0,1,2,3,4,5]}]},"DataReduction":{"DataVolume":3,"Primary":{"Window":{"Count":500}}},"Version":1},"ExecutionMetricsKind":1}}]},"QueryId":"","ApplicationContext":{"DatasetId":"5268a522-8bbd-41a0-82c9-c842f7b82739","Sources":[{"ReportId":"f13b6b6f-82a9-40ad-a7ba-64c3c6e7565e","VisualId":"c8ae720f712bb1124e47"}]}}],"cancelQueries":[],"modelId":1961083}'
 
     http = requests.Session()
     headers = {
@@ -46,6 +46,7 @@ json = json[0]
 json = json['DM0']
 
 is_first = True
+deaths = 0
 for item in json:
     data = item['C']
     r = None
@@ -56,6 +57,7 @@ for item in json:
         date = int(data[0]) / 1000
         dd.datetime = datetime.datetime.utcfromtimestamp(date).isoformat()
         if not r:
+            deaths += data[2]
             dd.cases = data[3]
             dd.hospitalized = data[4]
             dd.icu = data[5]
@@ -63,25 +65,47 @@ for item in json:
             dd.cases = data[2]
             dd.hospitalized = data[3]
             dd.icu = data[4]
+            # interesting...
+            deaths += 1
         elif r == 6:
             dd.cases = data[2]
             dd.hospitalized = data[3]
+        elif r == 12:
+            dd.hospitalized = data[2]
+            dd.icu = data[3]
+        elif r == 14:
+            dd.hospitalized = data[1]
+            dd.icu = data[2]
         elif r == 16:
             dd.cases = data[3]
             dd.icu = data[4]
+        elif r == 18:
+            dd.hospitalized = data[2]
         elif r == 20:
+            dd.cases = data[2]
+            dd.hospitalized = data[3]
+        elif r == 32:
+            deaths += data[2]
+            dd.cases = data[3]
+            dd.hospitalized = data[4]
+        elif r == 34:
             dd.cases = data[2]
             dd.hospitalized = data[3]
         elif r == 36:
             dd.cases = data[2]
             dd.hospitalized = data[3]
+        elif r == 38:
+            dd.cases = data[1]
+            dd.hospitalized = data[2]
+        elif r == 40:
+            deaths += data[2]
+            dd.hospitalized = data[3]
         elif r == 44:
             dd.hospitalized = data[2]
         elif r == 46:
             dd.hospitalized = data[1]
-        elif r == 38:
-            dd.cases = data[1]
-            dd.hospitalized = data[2]
+        elif r == 48:
+            dd.cases = data[3]
         elif r == 52:
             dd.cases = data[2]
         elif r == 54:
@@ -91,9 +115,10 @@ for item in json:
         elif r == 62:
             pass
         else:
-            print(f'>>> unexpected r: {r}')
+            print(f'>>> unexpected r: {r} @ {dd.datetime}')
 
         if dd:
+            dd.deaths = deaths
             if not is_first:
                 print('-' * 10)
             is_first = False
