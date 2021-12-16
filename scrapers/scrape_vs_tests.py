af75010c0f1d5f9900c527091c604d32b2e63d39
Markus Meier <maekke@gentoo.org>
2021-12-16 18:37:24 +0100

    remove ' separator values in VS tests scraper

--- scrapers/scrape_vs_tests.py
+++ scrapers/scrape_vs_tests.py
@@ -17,6 +17,7 @@ for url in svc.get_vs_weekly_pdf_urls():
         content = sc.pdftotext(pdf, page=page, raw=True)
         content = re.sub(r'(\d)\‘(\d)', r'\1\2', content)
         content = re.sub(r'(\d)\’(\d)', r'\1\2', content)
+        content = re.sub(r'(\d)\'(\d)', r'\1\2', content)
 
         td.total_tests = sc.find(r'Alle\s+Arten\s+von\s+Tests\s+(\d+)', content)
         td.positivity_rate = sc.find(r'Alle\s+Arten\s+von\s+Tests\s+\d+\s+(\d+\.\d+)%', content)
